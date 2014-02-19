e33e82c424a7cab68b3e9525aae01ed4f220fcf4
lemessurier <alemessurier@backflipstudios.com>
2014-02-19 15:47:48 -0700

    added homepage url for panda formula

--- panda.rb
+++ panda.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Panda < Formula
-  homepage ""
+  homepage "brew.backflipstudios.com/brew"
   url "http://brew.backflipstudios.com/brew/panda-1.0.tar.gz"
   sha1 "49e1117f6eb6f17db3bec2406db2a40de9f178ab"
 
