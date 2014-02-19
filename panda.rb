e5e075a8a2e7469d1f0a05a9293506d685b3d365
lemessurier <alemessurier@backflipstudios.com>
2014-02-19 15:45:54 -0700

    added formula for panda 1.0

---
+++ panda.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Panda < Formula
+  homepage ""
+  url "http://brew.backflipstudios.com/brew/panda-1.0.tar.gz"
+  sha1 "49e1117f6eb6f17db3bec2406db2a40de9f178ab"
+
+  def install
+	system "pwd"
+	puts "installing panda..."
+        system "mkdir -p ~/Documents/pandatest"
+        system "tar -xzvf panda-1.0.tar.gz ~/Documents/pandatest"
+        puts " ...done"
+  end
+end
