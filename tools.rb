c17e391aa3ea35eb1b14d6e7143aa06e69e82521
lemessurier <alemessurier@backflipstudios.com>
2014-02-19 18:32:48 -0700

    fixed install path for tools formula and added remove functionality

--- tools.rb
+++ tools.rb
@@ -5,10 +5,21 @@ class Tools < Formula
   url "http://brew.backflipstudios.com/brew/tools-1.0.tar.gz"
   sha1 "af2ddc319e7779a043b9382ec40cff502d6e2880"
 
+  @installpath = "~/bfs/utils/tools"
+
   def install
     puts "installing tools..."
 
-    system "cp artifacts.py ~/Documents"
+    system "mkdir #{installpath}"
+    system "cp artifacts.py #{installpath}"
+
+    puts " ...done"
+  end
+
+  def remove
+    puts "removing tools..."
+
+    system "rm -rf #{installpath}"
 
     puts " ...done"
   end
