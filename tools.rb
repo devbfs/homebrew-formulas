7ac2bc2cf729654c17b68062398bad80f23e2999
lemessurier <alemessurier@backflipstudios.com>
2014-02-19 18:55:19 -0700

    fixed syntax errors in tools formula

--- tools.rb
+++ tools.rb
@@ -5,13 +5,16 @@ class Tools < Formula
   url "http://brew.backflipstudios.com/brew/tools-1.0.tar.gz"
   sha1 "af2ddc319e7779a043b9382ec40cff502d6e2880"
 
-  @installpath = "~/bfs/utils/tools"
+  # @installpath = "~/bfs/utils/tools"
 
   def install
     puts "installing tools..."
 
-    system "mkdir #{installpath}"
-    system "cp artifacts.py #{installpath}"
+    # system "mkdir #{installpath}"
+    # system "cp artifacts.py #{installpath}"
+
+    system "mkdir ~/bfs/utils/tools"
+    system "cp -rf * ~/bfs/utils/tools"
 
     puts " ...done"
   end
@@ -19,7 +22,8 @@ class Tools < Formula
   def remove
     puts "removing tools..."
 
-    system "rm -rf #{installpath}"
+    # system "rm -rf #{installpath}"
+    system "rm -rf ~/bfs/utils/tools"
 
     puts " ...done"
   end
