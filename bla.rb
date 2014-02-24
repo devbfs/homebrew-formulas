ed0905fe0de90dacf1cf8f101dc25a24d81640cc
lemessurier <alemessurier@backflipstudios.com>
2014-02-24 10:55:39 -0700

    added new formula: bla.rb

--- bla.rb
+++ bla.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Bla < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/bla-1.2.tar.gz'
-  sha1 '97d9672cdc4ce5b2558a731a88b7462aad7ee4c2'
+  sha1 'efccc76146a3bc4d99c7c599930aa4acacb12df2'
 
   def install
     system 'sh config.sh'
