b01a603e537e08fa937825790301ef65b8050621
lemessurier <alemessurier@backflipstudios.com>
2014-02-25 10:31:52 -0700

    removed dash from ruby class name in backflip tools formula

--- backflip-tools.rb
+++ backflip-tools.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Backflip-tools < Formula
+class BackflipTools < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/backflip-tools-1.0.tar.gz'
   sha1 '1be69bd23b48a2a86272cf3f96f6fca25bf18a2a'
