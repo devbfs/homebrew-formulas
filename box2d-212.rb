435232cda6d3f2db5940ec9afc90d8c3dbc112e2
lemessurier <alemessurier@backflipstudios.com>
2014-02-25 17:00:59 -0700

    added new formula: box2d-212.rb

---
+++ box2d-212.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Box2d212 < Formula
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/box2d-212-1.0.tar.gz'
+  sha1 '519e9a5f7c9775158a1d9ae0fff2f4e8da319c30'
+
+  def install
+    print Dir.pwd
+    system 'sh config.sh'
+  end
+end
