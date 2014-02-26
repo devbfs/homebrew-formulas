c3e18a76de749ba885b88b2268bd3741c77bc0db
lemessurier <alemessurier@backflipstudios.com>
2014-02-26 15:11:22 -0700

    removed useless cbe package

--- panda.rb
+++ panda.rb
@@ -6,15 +6,6 @@ class Panda < Formula
   sha1 "49e1117f6eb6f17db3bec2406db2a40de9f178ab"
 
   def install
-      puts "installing panda..."
-      system "mkdir -p /usr/local/bfs/util/panda"
-      system "cp -rf * /usr/local/bfs/util/panda"
-      puts " ...done"
-  end
-
-  def uninstall
-      puts "removing panda..."
-      system "rm -rf /usr/local/bfs/util/panda"
-      puts " ...done"
+    prefix.install Dir['*']
   end
 end
