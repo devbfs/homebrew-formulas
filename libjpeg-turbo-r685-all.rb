9f360dc2631d41017e48199a08761ddba92bfbf0
lemessurier <alemessurier@backflipstudios.com>
2014-03-04 14:49:25 -0700

    added corrected formula for libjpeg turbo

--- libjpeg-turbo-r685-all.rb
+++ libjpeg-turbo-r685-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class LibjpegTurboR685All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/libjpeg-turbo-r685-all.tar.gz'
-  sha1 '9bca9ca0e73f1c56dabab59d6a92b05592e3e1c7'
+  sha1 '5ef125f9321f7f91636343fb4d8a15cb45d89ac6'
 
   def install
     prefix.install Dir['*']
