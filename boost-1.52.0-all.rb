76864b308df0b3dfc394047aee5b3ec637637bcc
lemessurier <alemessurier@backflipstudios.com>
2014-03-06 14:40:47 -0700

    added formula for boost 1.52.0

--- boost-1.52.0-all.rb
+++ boost-1.52.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Boost1520All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/boost-1.52.0-all.tar.gz'
-  sha1 '530359426d3f21ea7be8736e4dcc9bb226de3d48'
+  sha1 'c89c9f10cf6e20cde1bb201da7d782a5ff8d3fd0'
 
   def install
     prefix.install Dir['*']
