1d51708c3cd9cc558444bd735abbf9cf26bb286e
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 14:03:05 -0600

    updated eigen keg to remove extra dir layer

--- eigen-3.0.5-all.rb
+++ eigen-3.0.5-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Eigen305All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/eigen-3.0.5-all.tar.gz'
-  sha1 '54db4b13733c28019dd9f798f4ae521f68d780c3'
+  sha1 'd788c69ba01db5662ff8d92c5b866b7b662111db'
 
   def install
     prefix.install Dir['*']
