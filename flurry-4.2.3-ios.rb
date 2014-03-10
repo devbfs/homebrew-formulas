0f8fc4338c5e2375a67192e05cbe6839615de167
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 10:37:01 -0600

    created flurry kegs with improved internal dir structure

--- flurry-4.2.3-ios.rb
+++ flurry-4.2.3-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Flurry423Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/flurry-4.2.3-ios.tar.gz'
-  sha1 'a96b735b9bc101fef80344e77fe05bb5ebcbe0b1'
+  sha1 '32175eec942bb9350a59bf695684e09522fe7ba4'
 
   def install
     prefix.install Dir['*']
--- flurry-4.3.2-ios.rb
+++ flurry-4.3.2-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Flurry432Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/flurry-4.3.2-ios.tar.gz'
-  sha1 '4958c943238aefc2faa67017a2d95d963fb96c05'
+  sha1 'f92a1ded4169b6b66940ffe77f0349964155d4bc'
 
   def install
     prefix.install Dir['*']
