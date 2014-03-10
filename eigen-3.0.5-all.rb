eb7ac28cb9ad2cb401d0d8737f6daf42cfaf22a5
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 13:45:04 -0600

    removed 2 versions of eigen and added version with corrected dir structure

--- eigen-3.0.5-all.rb
+++ eigen-3.0.5-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Eigen305All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/eigen-3.0.5-all.tar.gz'
-  sha1 'eb7117b5dd477e665830eff9d9f4c1202dc5bb2e'
+  sha1 '54db4b13733c28019dd9f798f4ae521f68d780c3'
 
   def install
     prefix.install Dir['*']
