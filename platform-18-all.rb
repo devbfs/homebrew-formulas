591dc04a022e1bea70ac527e1e12f73b2419600e
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 09:30:40 -0600

    updated platform 18 keg to latest hg version

--- platform-18-all.rb
+++ platform-18-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Platform18All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-18-all.tar.gz'
-  sha1 'ea1356ff17feee44eae2729cbc535154dc6601a0'
+  sha1 '960a79a3dbcf1a7d142a3e1c89c862dfccb6700d'
 
   def install
     prefix.install Dir['*']
