d0e4c0c1839f6df9937b721e92b886798505c08f
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 14:17:27 -0600

    improved dir structure of libjpeg-turbo keg

--- libjpeg-turbo-r685-all.rb
+++ libjpeg-turbo-r685-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class LibjpegTurboR685All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/libjpeg-turbo-r685-all.tar.gz'
-  sha1 '5ef125f9321f7f91636343fb4d8a15cb45d89ac6'
+  sha1 '761b896a47fbf1a68aca553371aafcff45bd21f1'
 
   def install
     prefix.install Dir['*']
