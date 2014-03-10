ff7cbdbf4ae6659deb0f8200bdcffca55378266c
lemessurier <alemessurier@backflipstudios.com>
2014-03-10 15:55:35 -0600

    corrected internal structure of trex keg

--- trex-1.3-all.rb
+++ trex-1.3-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Trex13All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/trex-1.3-all.tar.gz'
-  sha1 '8cb100a7c7ce41b950f0bb8c67bbb3a89f632a92'
+  sha1 '09e43217b8c46106a4deda4dcee4c339c4d23479'
 
   def install
     prefix.install Dir['*']
