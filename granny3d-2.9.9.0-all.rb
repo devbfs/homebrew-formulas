6211c6526f53c6badccd844399a071ef7b485330
lemessurier <alemessurier@backflipstudios.com>
2014-03-11 15:06:06 -0600

    fixed internal structure of granny3d keg

--- granny3d-2.9.9.0-all.rb
+++ granny3d-2.9.9.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Granny3d2990All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/granny3d-2.9.9.0-all.tar.gz'
-  sha1 '48d3ba05fb74cceebfbe1471c35bc36666aea286'
+  sha1 '628a6ee2706fb44b32cd4b49e55b306f3cd2ef96'
 
   def install
     prefix.install Dir['*']
