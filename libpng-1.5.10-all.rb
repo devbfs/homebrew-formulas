9cb2b786d6178b1d5bc36ee70631a8843a444d43
lemessurier <alemessurier@backflipstudios.com>
2014-03-11 14:24:13 -0600

    new libpng formula with corrected dir structure

--- libpng-1.5.10-all.rb
+++ libpng-1.5.10-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libpng1510All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/libpng-1.5.10-all.tar.gz'
-  sha1 '9c9b14e687b6ff2f56cfa2a142b595861ccdcbb0'
+  sha1 'ac7217401412833f33c3092ce588fb909b2e241d'
 
   def install
     prefix.install Dir['*']
