d7612254f8cc95cc469dcacc1ea8eab3e0d74b27
lemessurier <alemessurier@backflipstudios.com>
2014-03-11 14:53:57 -0600

    fixed dir structure for harfbuzz keg

--- harfbuzz-0.6.0-all.rb
+++ harfbuzz-0.6.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Harfbuzz060All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/harfbuzz-0.6.0-all.tar.gz'
-  sha1 '8ab520a17a3f403c78320a9f611aa846b6f0e950'
+  sha1 'd403f614ef2f8cd37f5cbfedd10b5ff69b9ac283'
 
   def install
     prefix.install Dir['*']
