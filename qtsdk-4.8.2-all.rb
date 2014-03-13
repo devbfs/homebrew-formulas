5759eacac73242616f16fd637ad7139fdf74435f
lemessurier <alemessurier@backflipstudios.com>
2014-03-13 10:58:22 -0600

    added formula for qt sdk

---
+++ qtsdk-4.8.2-all.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Qtsdk482All < Formula
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/qtsdk-4.8.2-all.tar.gz'
+  sha1 '4ba365791109dc1ab4d3e0d578acafa25f75cbdc'
+
+  def install
+    prefix.install Dir['*']
+  end
+end
