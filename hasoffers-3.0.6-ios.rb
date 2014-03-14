a2dac035bc8f2265d678e7ea85bf74df3105bc8c
lemessurier <alemessurier@backflipstudios.com>
2014-03-14 15:46:58 -0600

    fixed dir structure of hasoffers keg

--- hasoffers-3.0.6-ios.rb
+++ hasoffers-3.0.6-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Hasoffers306Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/hasoffers-3.0.6-ios.tar.gz'
-  sha1 '25eefe26df1da9262a8f610a0369686fc1a30962'
+  sha1 'a9f4930f89fc60e7810a1d73de0755fa0cfe37bd'
 
   def install
     prefix.install Dir['*']
