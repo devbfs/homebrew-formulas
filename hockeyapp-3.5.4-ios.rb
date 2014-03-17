2c4e7b2fdd0e7daa10afc4c30df0c2fbb5ef72ff
lemessurier <alemessurier@backflipstudios.com>
2014-03-17 16:34:23 -0600

    corrected dir structure in hockeyapp 3.5.4 keg

--- hockeyapp-3.5.4-ios.rb
+++ hockeyapp-3.5.4-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Hockeyapp354Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/hockeyapp-3.5.4-ios.tar.gz'
-  sha1 '6274770f9336e9ae860dcb9b30fc98d2091e725f'
+  sha1 '4e403794d6dd00e0774da120c2b220ecbbc86737'
 
   def install
     prefix.install Dir['*']
