afdd5b55a56910f70e7d05113ce000f16027dcd3
lemessurier <alemessurier@backflipstudios.com>
2014-03-19 15:10:34 -0600

    added boost-1.54.0-all dependency to platform 18 formula

--- platform-18-all.rb
+++ platform-18-all.rb
@@ -4,6 +4,7 @@ class Platform18All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-18-all.tar.gz'
   sha1 '960a79a3dbcf1a7d142a3e1c89c862dfccb6700d'
+  depends_on 'boost-1.54.0-all'
 
   def install
     prefix.install Dir['*']
