cda010304acc8584d9392f9543d8ab1c1d4979e9
lemessurier <alemessurier@backflipstudios.com>
2014-03-20 16:18:47 -0600

    simplified dir structure in jsoncpp keg

--- jsoncpp-0.5.0-all.rb
+++ jsoncpp-0.5.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jsoncpp050All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/jsoncpp-0.5.0-all.tar.gz'
-  sha1 '85952e96192975a5e6ac5fec866a884b1d9e26ba'
+  sha1 'd5d2898cfb9c4d7aa1405c14bacb04dabea98048'
 
   def install
     prefix.install Dir['*']
