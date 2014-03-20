2138853e3d75dbfb7bfd18e6eeb11761236dae20
lemessurier <alemessurier@backflipstudios.com>
2014-03-20 15:05:26 -0600

    removed excessive version number subdir from agg 2.4 keg

--- agg-2.4-all.rb
+++ agg-2.4-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Agg24All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/agg-2.4-all.tar.gz'
-  sha1 '455581562360ae803ec89d05229dac7dcac62e14'
+  sha1 'a2b56449f94bc93e337a99e067f10d5c487ba0fa'
 
   def install
     prefix.install Dir['*']
