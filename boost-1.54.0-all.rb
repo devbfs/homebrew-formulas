40964232a58ff409e176ef12bc95e2b52de96fd4
lemessurier <alemessurier@backflipstudios.com>
2014-03-20 16:02:44 -0600

    removed excessive subdir from boost 1.54.0 keg

--- boost-1.54.0-all.rb
+++ boost-1.54.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Boost1540All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/boost-1.54.0-all.tar.gz'
-  sha1 'cc1f9b221ea80620ace6a81d7b66206791513697'
+  sha1 '53db846f3675b5dd6e0ba071b8cac251cb6884e2'
 
   def install
     prefix.install Dir['*']
