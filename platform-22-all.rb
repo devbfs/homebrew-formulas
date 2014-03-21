444b5c277f917ff855af90efe7ae249ba71aebd1
lemessurier <alemessurier@backflipstudios.com>
2014-03-20 18:19:23 -0600

    added boost-1.54.0-all dependency to platform 22 formula

--- platform-22-all.rb
+++ platform-22-all.rb
@@ -4,6 +4,7 @@ class Platform22All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-22-all.tar.gz'
   sha1 '7e3754c31c77ab7fd5ce65348eec5bbba12ef2d9'
+  depends_on 'boost-1.54.0-all'
 
   def install
     prefix.install Dir['*']
