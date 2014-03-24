88c2f6714ffd4b78c4cda04ca50f450e7e98e93e
lemessurier <alemessurier@backflipstudios.com>
2014-03-24 13:12:10 -0600

    corrected ridiculously redundant dir structure in ios flurry kegs

--- flurry-4.3.2-ios.rb
+++ flurry-4.3.2-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Flurry432Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/flurry-4.3.2-ios.tar.gz'
-  sha1 '9c60ecd1de4bad2963743ed3591f27014f79ec08'
+  sha1 '0750e6da0ebb3f3071ad126617397735aa74895b'
 
   def install
     prefix.install Dir['*']
