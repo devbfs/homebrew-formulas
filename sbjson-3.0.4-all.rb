553235421b1f509738aefdf3902b6fe881cd332c
lemessurier <alemessurier@backflipstudios.com>
2014-03-24 10:53:17 -0600

    fixed dir structure in sbjson keg and removed unnecessary one

--- sbjson-3.0.4-all.rb
+++ sbjson-3.0.4-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sbjson304All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/sbjson-3.0.4-all.tar.gz'
-  sha1 '5a6339cadfc22b2ef4cd1c37a68f9971ee916295'
+  sha1 'f39f0e15ffeb8ab52110a1a7125ec3cfa1678994'
 
   def install
     prefix.install Dir['*']
