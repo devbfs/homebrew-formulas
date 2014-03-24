ade2310deb28a69caf080077092ce04027ea37b7
lemessurier <alemessurier@backflipstudios.com>
2014-03-24 17:00:03 -0600

    added dependencies for platform 22

--- platform-22-all.rb
+++ platform-22-all.rb
@@ -5,6 +5,8 @@ class Platform22All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-22-all.tar.gz'
   sha1 '7e3754c31c77ab7fd5ce65348eec5bbba12ef2d9'
   depends_on 'boost-1.54.0-all'
+  depends_on 'hockeyapp-3.5.4-ios'
+  depends_on 'jansson-2.3.1House-all'
 
   def install
     prefix.install Dir['*']
