8977e860b12383ae3812cb929c5ac35b744b6d91
lemessurier <alemessurier@backflipstudios.com>
2014-03-24 10:36:30 -0600

    fixed dir structure in jansson keg, removed unnecessary non-house version of jansson

--- jansson-2.3.1House-all.rb
+++ jansson-2.3.1House-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jansson231houseAll < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/jansson-2.3.1House-all.tar.gz'
-  sha1 '4806fa2fdcbed5e3923389ed563ca214c8cdef4a'
+  sha1 '6a0e73286cc2c29e06a6507859e310f7e67fa929'
 
   def install
     prefix.install Dir['*']
