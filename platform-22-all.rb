467aa2aa20fdb516cfddaee02c1af6793ad810c7
lemessurier <alemessurier@backflipstudios.com>
2014-03-24 17:36:30 -0600

    updated platform 22 dependencies

--- platform-22-all.rb
+++ platform-22-all.rb
@@ -7,6 +7,10 @@ class Platform22All < Formula
   depends_on 'boost-1.54.0-all'
   depends_on 'hockeyapp-3.5.4-ios'
   depends_on 'jansson-2.3.1House-all'
+  depends_on 'facebook-3.5.1-ios'
+  depends_on 'facebook-4cbe4e2-android'
+  depends_on 'burstly-2.7.0.51093house-ios'
+  depends_on 'burstly-1.9.2.144-android'
 
   def install
     prefix.install Dir['*']
