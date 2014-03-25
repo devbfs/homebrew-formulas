71e5f1836574ee84d2993ca0874195ab897868ca
lemessurier <alemessurier@backflipstudios.com>
2014-03-25 10:45:53 -0600

    updated platform keg with revised dependency paths, fixed structure of nedmalloc keg

--- nedmalloc-105-all.rb
+++ nedmalloc-105-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Nedmalloc105All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/nedmalloc-105-all.tar.gz'
-  sha1 '02ec2a014ce81a2460abcc89ecfa0c89f58c6ff3'
+  sha1 'dcffa80afaffae2f44e30868a551d753495d4fd9'
 
   def install
     prefix.install Dir['*']
--- platform-22-all.rb
+++ platform-22-all.rb
@@ -3,20 +3,7 @@ require 'formula'
 class Platform22All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-22-all.tar.gz'
-  sha1 '7e3754c31c77ab7fd5ce65348eec5bbba12ef2d9'
-  depends_on 'boost-1.54.0-all'
-  depends_on 'hockeyapp-3.5.4-ios'
-  depends_on 'jansson-2.3.1House-all'
-  depends_on 'facebook-3.5.1-ios'
-  depends_on 'facebook-4cbe4e2-android'
-  depends_on 'burstlyhouse-2.7.0.51093-ios'
-  depends_on 'burstlyfull-2.7.0.51093-ios'
-  depends_on 'burstly-1.9.2.144-android'
-  depends_on 'fmod-4.44.12-ios'
-  depends_on 'fmod-4.44.31-android'
-  depends_on 'jsoncpp-0.5.0-all'
-  depends_on 'flurry-4.3.2-ios'
-  depends_on 'flurry-3.0.5-android'
+  sha1 '7b049e6a01a9df74a4eba01c3a27220e3a583227'
 
   def install
     prefix.install Dir['*']
