00a1ff732f3980a77c6066fc2fce3fc54b2db39b
lemessurier <alemessurier@backflipstudios.com>
2014-03-25 10:56:34 -0600

    corrected platform keg structure

--- platform-22-all.rb
+++ platform-22-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Platform22All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-22-all.tar.gz'
-  sha1 '7b049e6a01a9df74a4eba01c3a27220e3a583227'
+  sha1 '6e198f1a857720049f37696d4eaabff929d8f5cc'
 
   def install
     prefix.install Dir['*']
