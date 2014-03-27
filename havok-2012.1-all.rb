0fb8de48f8c86149a0fb6e073c7e8245430e7a79
lemessurier <alemessurier@backflipstudios.com>
2014-03-27 13:09:25 -0600

    corrected dir structure of havok 2012.1 keg

--- havok-2012.1-all.rb
+++ havok-2012.1-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Havok20121All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/havok-2012.1-all.tar.gz'
-  sha1 '9acb188f5582725b9e31159b34d383bdf1b6007c'
+  sha1 '923750cdf25b398d15aaa5fe9fa03531ea6b1d15'
 
   def install
     prefix.install Dir['*']
