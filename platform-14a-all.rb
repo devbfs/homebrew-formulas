1b7f64aa107f17b46f5062612806cbb07943ef81
lemessurier <alemessurier@backflipstudios.com>
2014-04-01 10:47:07 -0600

    testing post uninstall function in platform 14a formula

--- platform-14a-all.rb
+++ platform-14a-all.rb
@@ -3,13 +3,13 @@ require 'formula'
 class Platform14aAll < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-14a-all.tar.gz'
-  sha1 '0510d8f3d67cb871cd88e663fd457cf7af583239'
+  sha1 '2f84308ab7cb659a45c73ffe621576f0f5b8e97a'
 
   def install
     prefix.install Dir['*']
   end
 
   def post_uninstall
-    puts "testing post_uninstall function..."
+    puts "testing post_uninstall..."
   end
 end
