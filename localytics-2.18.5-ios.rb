ddaf7763e9bb19d59c0a68cdc7b6a2081b276741
lemessurier <alemessurier@backflipstudios.com>
2014-04-09 12:36:01 -0600

    correct dir structure of localytics keg

--- localytics-2.18.5-ios.rb
+++ localytics-2.18.5-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Localytics2185Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/localytics-2.18.5-ios.tar.gz'
-  sha1 '470a30668e3577db82f61e1b829aef7325be204e'
+  sha1 '97f8c1ae604b8ba55aa7ade6cf1097391a28606d'
 
   def install
     prefix.install Dir['*']
