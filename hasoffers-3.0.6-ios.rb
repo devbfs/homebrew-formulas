5ac45ee91c3fc57eda0af4b69d9cbf159ad0d599
lemessurier <alemessurier@backflipstudios.com>
2014-04-09 13:02:46 -0600

    added symlink in hasoffers keg to accomodate bizzare include path scheme in dwarvenden

--- hasoffers-3.0.6-ios.rb
+++ hasoffers-3.0.6-ios.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Hasoffers306Ios < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/hasoffers-3.0.6-ios.tar.gz'
-  sha1 '95d54e7721ed5c7c3bed0dea92d274274046880a'
+  sha1 '43e5e2148e508e419ab8b6ec2a5bfa6b5ec13054'
 
   def install
     prefix.install Dir['*']
