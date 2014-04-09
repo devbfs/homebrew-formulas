c5c7abcfa85e5a45e4216ed87f2bd20d51b318fd
lemessurier <alemessurier@backflipstudios.com>
2014-04-09 17:07:35 -0600

    updated archon kegs to include recent bugfixes (no change in version numbers...)

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Archon029All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 'f64b28f00e033e3ebbd384087ac63b2244b9a878'
+  sha1 '6517d070a45d4b9a45effacc16d35bfbc97f1119'
 
   def install
     prefix.install Dir['*']
--- archon-0.3.1-all.rb
+++ archon-0.3.1-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Archon031All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.3.1-all.tar.gz'
-  sha1 '5f6540d23a1bc01fd19a3cae9cfabc309a74fe5e'
+  sha1 '01d967b85640271e6bca3e4033722c53b2982958'
 
   def install
     prefix.install Dir['*']
