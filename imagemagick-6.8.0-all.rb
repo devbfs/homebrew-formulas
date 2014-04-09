4b583798f3efc507ebd8bb449533ba62cc821936
lemessurier <alemessurier@backflipstudios.com>
2014-04-09 17:21:38 -0600

    created keg for imagemagick 6.8 used by archon

---
+++ imagemagick-6.8.0-all.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Imagemagick680All < Formula
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/imagemagick-6.8.0-all.tar.gz'
+  sha1 '833e5da0928f26a0dda1b5f2ac17937b30554caa'
+
+  def install
+    prefix.install Dir['*']
+  end
+end
