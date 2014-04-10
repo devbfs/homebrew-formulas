aae80a6e0ba4c52e1fa715bad74fb3b4f14e74d5
lemessurier <alemessurier@backflipstudios.com>
2014-04-10 11:40:21 -0600

    made fontforge keg keg_only to avoid linking conflicts

--- fontforge-20120731BFS-all.rb
+++ fontforge-20120731BFS-all.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Fontforge20120731bfsAll < Formula
+  keg_only 'This keg is only used by archon, which will know where to look automatically.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/fontforge-20120731BFS-all.tar.gz'
   sha1 'b9c1a66f7ec53b0a07bb826a5a7465273a03bf3e'
