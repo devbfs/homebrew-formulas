ac00997fad9ce5fa528b0d3b5882729d0237b535
lemessurier <alemessurier@backflipstudios.com>
2014-04-10 13:59:21 -0600

    corrected URL in formulas to reflect platform change

--- imagemagick-6.8.0-mac.rb
+++ imagemagick-6.8.0-mac.rb
@@ -4,7 +4,7 @@ class Imagemagick680Mac < Formula
   keg_only 'This is an in-house keg. Anything that needs it should be able to find it automatically.'
 
   homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/imagemagick-6.8.0-all.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/imagemagick-6.8.0-mac.tar.gz'
   sha1 '833e5da0928f26a0dda1b5f2ac17937b30554caa'
 
   def install
