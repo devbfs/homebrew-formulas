3a46282319a005dc9c0091313589f5de872d4ba0
lemessurier <alemessurier@backflipstudios.com>
2014-04-10 12:53:11 -0600

    replaced references to thirdparty with cellar references in archon keg

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,14 +3,7 @@ require 'formula'
 class Archon029All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 '6517d070a45d4b9a45effacc16d35bfbc97f1119'
-
-  depends_on 'pvrtextool-2.09.29.0678-all'
-  depends_on 'imagemagick-6.8.0-all'
-  depends_on 'havok-2012.1-all'
-  depends_on 'lua-1.0-all'
-  depends_on 'fontforge-20120731BFS-all'
-  depends_on 'protobuf-2.4.1python-all'
+  sha1 '526a77971961277a23ad97c07ad4d0e558f26361'
 
   def install
     prefix.install Dir['*']
