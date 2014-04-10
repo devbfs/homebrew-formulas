022be3840f67f894ed498a56622d1e5486070be1
lemessurier <alemessurier@backflipstudios.com>
2014-04-10 12:54:01 -0600

    re-added dependencies to archon keg

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -5,6 +5,13 @@ class Archon029All < Formula
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
   sha1 '526a77971961277a23ad97c07ad4d0e558f26361'
 
+  depends_on 'pvrtextool-2.09.29.0678-all'
+  depends_on 'imagemagick-6.8.0-all'
+  depends_on 'havok-2012.1-all'
+  depends_on 'lua-1.0-all'
+  depends_on 'fontforge-20120731BFS-all'
+  depends_on 'protobuf-2.4.1python-all'
+
   def install
     prefix.install Dir['*']
   end
