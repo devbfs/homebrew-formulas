c8d1773600a6d0e46179da5eca61dabd427576f6
lemessurier <alemessurier@backflipstudios.com>
2014-04-14 16:10:10 -0600

    tweaks to unity/xcode formulas

--- unity-4.3.4f1-mac.rb
+++ unity-4.3.4f1-mac.rb
@@ -9,6 +9,6 @@ class Unity434f1Mac < Formula
     prefix.install Dir['*']
     puts 'You will need to manually create a symlink fgor this keg since modifying /Applications requires root permissions.'
     puts 'Assuming there is no existing regular (non-brew) installation of Unity, use this command:'
-    puts 'ln -s -f /usr/local/Cellar/unity-4.3.4f1-all/4.3.4f1/Unity.app /Applications/Unity.app'
+    puts 'ln -s -f /usr/local/Cellar/unity-4.3.4f1-mac/4.3.4f1/Unity.app /Applications/Unity.app'
   end
 end
--- xcode-5.1-mac.rb
+++ xcode-5.1-mac.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Xcode51Mac < Formula
   homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/xcode-5.1-all.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/xcode-5.1-mac.tar.gz'
   sha1 '5585a65a5f8d7070cd685722b97597c87abcd4c7'
 
   def install
