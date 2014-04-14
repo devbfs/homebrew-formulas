fde9e976ddb7858315319f198d2a67c42b7e8da0
lemessurier <alemessurier@backflipstudios.com>
2014-04-14 16:25:14 -0600

    made imagemagick formula keg only, which will prevent it from conflicting with other versions

--- imagemagick-6.8.0-mac.rb
+++ imagemagick-6.8.0-mac.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Imagemagick680Mac < Formula
+  keg_only 'This keg is only used by archon, which will know where to look automatically.'
+  
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/imagemagick-6.8.0-mac.tar.gz'
   sha1 '2999a84c16383cb2e7a743153dfe481d8eb00b36'
