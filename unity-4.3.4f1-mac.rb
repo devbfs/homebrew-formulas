c182e1e4f358f9cdb687f622ba0b64db01d0f7c7
lemessurier <alemessurier@backflipstudios.com>
2014-04-14 15:57:13 -0600

    corrected unity platform

--- unity-4.3.4f1-mac.rb
+++ unity-4.3.4f1-mac.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
-class Unity434f1All < Formula
+class Unity434f1Mac < Formula
   homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/unity-4.3.4f1-all.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/unity-4.3.4f1-mac.tar.gz'
   sha1 '8749b1ff9778a80e60edcf45897884f3a9697d47'
 
   def install
