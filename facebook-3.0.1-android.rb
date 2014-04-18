1195daa8727dcad2cbb625845a78ebc1dca1bba2
lemessurier <alemessurier@backflipstudios.com>
2014-04-18 15:07:35 -0600

    added keg for facebook sdk 3.0.1

--- facebook-3.0.1-android.rb
+++ facebook-3.0.1-android.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Facebook301Android < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/facebook-3.0.1-android.tar.gz'
-  sha1 'e80006e928b2c7ac70de0a11de2e407bca4f02ee'
+  sha1 '1d4f106e72ec969f4c9236f13a22020f24ecfa8b'
 
   def install
     prefix.install Dir['*']
