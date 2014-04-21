ca6e9f36e086ed9d826221992b5a9926ef8c19c8
lemessurier <alemessurier@backflipstudios.com>
2014-04-21 14:20:50 -0600

    rolled python component into main protobuf 2.4.1 keg

--- protobuf-2.4.1-all.rb
+++ protobuf-2.4.1-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Protobuf241All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/protobuf-2.4.1-all.tar.gz'
-  sha1 '98f021304ca4b3b60c6b01c733aa6d5d8cb2f9ee'
+  sha1 '5e64e5bebbbc36a2b5f313a6c3b46c69a562407b'
 
   def install
     prefix.install Dir['*']
