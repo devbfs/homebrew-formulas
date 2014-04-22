c4ad9a7ee1269e46902adec09f8431cdbf3573a9
lemessurier <alemessurier@backflipstudios.com>
2014-04-22 14:54:04 -0600

    updated harfbuzz keg

--- harfbuzz-0.6.0-all.rb
+++ harfbuzz-0.6.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Harfbuzz060All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/harfbuzz-0.6.0-all.tar.gz'
-  sha1 'd403f614ef2f8cd37f5cbfedd10b5ff69b9ac283'
+  sha1 '23fd20c054d393358c229bed7d586d5998f9a7ed'
 
   def install
     prefix.install Dir['*']
