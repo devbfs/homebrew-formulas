8c153a990f333c970bd595a3d9664034603ee432
lemessurier <alemessurier@backflipstudios.com>
2014-04-23 16:30:49 -0600

    corrected dependencies for archon kegs, removed trailing whitespace from qt keg since for some reason brew cares

--- archon-0.2.8-all.rb
+++ archon-0.2.8-all.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Archon028All < Formula
+  keg_only 'Anything that needs this will know where to find it.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.2.8-all.tar.gz'
   sha1 '3ff759d6ed6fd408378162f753845cd9e59c5b51'
@@ -10,7 +12,7 @@ class Archon028All < Formula
   depends_on 'havok-2012.1-all'
   depends_on 'lua-1.0-all'
   depends_on 'fontforge-20120731BFS-all'
-  depends_on 'protobuf-2.4.1python-all'
+  depends_on 'protobuf-2.4.1-all'
   depends_on 'granny3dpreprocessor-1.0-mac'
 
   def install
--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Archon029All < Formula
+  keg_only 'Anything that needs this will know where to find it.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
   sha1 '526a77971961277a23ad97c07ad4d0e558f26361'
@@ -10,7 +12,7 @@ class Archon029All < Formula
   depends_on 'havok-2012.1-all'
   depends_on 'lua-1.0-all'
   depends_on 'fontforge-20120731BFS-all'
-  depends_on 'protobuf-2.4.1python-all'
+  depends_on 'protobuf-2.4.1-all'
   depends_on 'granny3dpreprocessor-1.0-mac'
 
   def install
