fcbcd341e9db3eced53b8db2b34a513fa246fd08
lemessurier <alemessurier@backflipstudios.com>
2014-04-24 15:48:53 -0600

    re-deploy of mopub keg. investigating strange permission issue

--- mopub-1.15.2.2-android.rb
+++ mopub-1.15.2.2-android.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Mopub11522Android < Formula
+  keg_only 'Anything that needs this will know where to find it.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/mopub-1.15.2.2-android.tar.gz'
-  sha1 '6411ed3347f1ccd937fbb95ab1cff60c9ba65443'
+  sha1 '20ba3a20e8c873a2ec831e769b906b21b6786357'
 
   def install
     prefix.install Dir['*']
