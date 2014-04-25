010307624555db78386772c91c6a9724a866064d
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 13:32:42 -0600

    added hash for stable version of backflip brew tools

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -4,6 +4,7 @@ class BackflipBrewTools < Formula
   homepage 'http://brew.backflipstudios.com'
   head 'https://github.com/devbfs/brew-util.git', :revision => '29b9035'
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
+  sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
 
   def install
     prefix.install Dir['*']
