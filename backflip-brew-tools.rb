98c006220ac51386a39fa4e69790715120279774
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 13:38:47 -0600

    added version number for tools formula

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -5,6 +5,7 @@ class BackflipBrewTools < Formula
   head 'https://github.com/devbfs/brew-util.git'
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
   sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
+  version '1.0'
 
   def install
     prefix.install Dir['*']
