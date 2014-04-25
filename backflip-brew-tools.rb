29616f4c560ca1a75aa77edc6774e6cd91394bea
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 13:31:09 -0600

    specified revision 29b9035 in backflip-brew-tools formula

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   homepage 'http://brew.backflipstudios.com'
-  head 'https://github.com/devbfs/brew-util.git'
+  head 'https://github.com/devbfs/brew-util.git', :revision => '29b9035'
+  url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
 
   def install
     prefix.install Dir['*']
