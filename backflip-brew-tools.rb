622b6c2bdc8241e9766661e368991ce1a02e3df6
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 13:35:30 -0600

    removed unnecessary revision number from brew tools formula

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   homepage 'http://brew.backflipstudios.com'
-  head 'https://github.com/devbfs/brew-util.git', :revision => '29b9035'
+  head 'https://github.com/devbfs/brew-util.git'
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
   sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
 
