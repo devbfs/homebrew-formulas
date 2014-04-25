b13043e29464edf0c0ce33f3a415a1c5f940380e
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 12:41:14 -0600

    added keg for backflip brew tools

---
+++ backflip-brew-tools.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class BackflipBrewTools < Formula
+  homepage 'http://brew.backflipstudios.com'
+  url 'https://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
+  head 'https://github.com/devbfs/homebrew-formulas.git, :using => :git, :branch => master'
+  sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
+
+  def install
+    prefix.install Dir['*']
+  end
+end
