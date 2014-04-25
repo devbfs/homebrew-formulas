b76c48c771f9c234aa6f86f69ad85ac9d40fc06a
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 12:54:37 -0600

    adjusting backflip brew tools formula to pull from git repo

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,9 @@ require 'formula'
 class BackflipBrewTools < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  head 'https://github.com/devbfs/homebrew-formulas.git, :using => :git, :branch => master'
+  head 'https://github.com/devbfs/homebrew-formulas.git', :using => :git, :branch => master
   sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
+  version '1.0'
 
   def install
     prefix.install Dir['*']
