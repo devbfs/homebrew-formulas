73918e243f7fefff3e882dde828e07bca978480e
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-04-25 15:43:28 -0600

    Testing with backflip-brew-tools.rb

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class BackflipBrewTools < Formula
-  url 'https://github.com/devbfs/brew-util.git'
+  head 'https://github.com/devbfs/brew-util.git', :revision => "4a980495cd90c2e5e3f1d101f6045c9123e3dbf7"
 
   def install
     prefix.install Dir['*']
