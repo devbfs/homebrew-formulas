d575f12500c25f7025901c57e6e203df46addb62
lemessurier <alemessurier@backflipstudios.com>
2014-04-25 13:23:14 -0600

    cleanup in brew tools formula

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -2,10 +2,7 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   homepage 'http://brew.backflipstudios.com'
-  # url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
   head 'https://github.com/devbfs/brew-util.git'
-  # sha1 '461eb64bad07d6f062ebd16f98d962bd8300f750'
-  # version '1.0'
 
   def install
     prefix.install Dir['*']
