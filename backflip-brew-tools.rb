df0147d385444c002766cb394096abef13b99de8
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-04-30 16:48:23 -0600

    Updated backflip-brew-tools

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -2,7 +2,7 @@
 require 'formula'
 
 class BackflipBrewTools < Formula
-  url 'http://brew.backflipstudios.com/backflip-brew-tools.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
   sha1 '01c6e49d479b151b6051b36d346dc2cc15e40b35'
   version '3'
 
