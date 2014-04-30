a08b121f25fad5be7b4e95dfd1f01209764dde6a
lemessurier <alemessurier@backflipstudios.com>
2014-04-30 13:28:52 -0600

    removed service config script from platform keg. now located in the backflip-engine-support keg

--- platform-28-all.rb
+++ platform-28-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Platform28All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/platform-28-all.tar.gz'
-  sha1 '4184bda270303a22c4085248ce7958e203c342c6'
+  sha1 'd3e6a9595eff9fdc809fc298124bebab15fca1aa'
 
   depends_on 'boost-1.54.0-all'
   depends_on 'hockeyapp-3.5.4-ios'
