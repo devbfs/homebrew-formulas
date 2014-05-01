ebe9866956646d413a1bc58998565050ab9c7971
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-05-01 09:37:29 -0600

    backflip-brew-tools version 8

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,11 +3,10 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 '8cddf85fffcbe6976bc7c34fc4d96843b48c5814'
-  version '7'
+  sha1 '571e38369cd3276567754a87cbda492ac71ea911'
+  version '8'
 
   def install
     prefix.install Dir['*']
-    bin.install Dir['*']
   end
 end
