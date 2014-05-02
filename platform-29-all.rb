5cce627e510aedb2e64bf7802696ee24b94af749
lemessurier <alemessurier@backflipstudios.com>
2014-05-01 18:04:39 -0600

    added keg for platform 29

---
+++ platform-29-all.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Platform28All < Formula
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/platform-29-all.tar.gz'
+  sha1 '266eaad441e3780c75b8953e98e5e7c0fe86d967'
+
+  depends_on 'boost-1.54.0-all'
+  depends_on 'hockeyapp-3.5.4-ios'
+  depends_on 'jansson-2.3.1House-all'
+  depends_on 'facebook-3.5.1-ios'
+  depends_on 'jsoncpp-0.5.0-all'
+  depends_on 'flurry-4.3.2-ios'
+  depends_on 'flurry-3.0.5-android'
+  depends_on 'sbjson-3.0.4-all'
+  depends_on 'googleconversiontracking-3.0.0-ios'
+  depends_on 'googlemobileads-6.8.0-ios'
+
+  def install
+    prefix.install Dir['*']
+  end
+end
