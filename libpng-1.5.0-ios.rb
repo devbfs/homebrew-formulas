57ca8b0458ee2c64b5fc871c0a00e3c63b1b3e7a
lemessurier <alemessurier@backflipstudios.com>
2014-05-13 14:51:00 -0600

    made libpng 1.5.0 keg only to avoid link problems

--- libpng-1.5.0-ios.rb
+++ libpng-1.5.0-ios.rb
@@ -2,6 +2,8 @@
 require 'formula'
 
 class Libpng150Ios < Formula
+  keg_only 'Anything that needs this will know where to look.'
+
   url 'http://brew.backflipstudios.com/brew/libpng-1.5.0-ios.tar.gz'
   sha1 'b350f1ef8215f9e2b898d3f2f4e9ee58173009b6'
   version '1.5.0'
