e5cd786ac420959c7b17854b21b39ff2b2a48e20
lemessurier <alemessurier@backflipstudios.com>
2014-05-13 14:39:16 -0600

    created a formula for older ios/mac only libpng dir. strike knight appears to require this

---
+++ libpng-1.5.0-ios.rb
@@ -0,0 +1,12 @@
+
+require 'formula'
+
+class Libpng150Ios < Formula
+  url 'http://brew.backflipstudios.com/brew/libpng-1.5.0-ios.tar.gz'
+  sha1 'b350f1ef8215f9e2b898d3f2f4e9ee58173009b6'
+  version '1.5.0'
+
+  def install
+    prefix.install Dir['*']
+  end
+end
