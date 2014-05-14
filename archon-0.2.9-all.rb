2130855bc81443dcdd4376b9f4c6d35f606b0ab3
lemessurier <alemessurier@backflipstudios.com>
2014-05-14 14:21:38 -0600

    updated archon 0.2.9 keg with lots of fixed paths

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -1,19 +1,10 @@
+
 require 'formula'
 
 class Archon029All < Formula
-  keg_only 'Anything that needs this will know where to find it.'
-
-  homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 '526a77971961277a23ad97c07ad4d0e558f26361'
-
-  depends_on 'pvrtextool-2.09.29.0678-mac'
-  depends_on 'imagemagick-6.8.0-mac'
-  depends_on 'havok-2012.1-all'
-  depends_on 'lua-1.0-all'
-  depends_on 'fontforge-20120731BFS-all'
-  depends_on 'protobuf-2.4.1-all'
-  depends_on 'granny3dpreprocessor-1.0-mac'
+  sha1 '1fcff7ea910fe05bd57bc203e472d7361a725c80'
+  version '0.2.9'
 
   def install
     prefix.install Dir['*']
