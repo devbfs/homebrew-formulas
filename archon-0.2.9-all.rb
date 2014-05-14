a4442589e90089c5ac85f129285f3598e4f0ad4f
lemessurier <alemessurier@backflipstudios.com>
2014-05-14 14:36:47 -0600

    fixed archon formula (had wrong archive)

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 
 class Archon029All < Formula
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 '1fcff7ea910fe05bd57bc203e472d7361a725c80'
+  sha1 'd7772e7fa26e01308f945b8cfdd5661af5b76270'
   version '0.2.9'
 
   def install
