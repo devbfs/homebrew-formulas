6752c1f40ebea12689a96a2e475ca761d575bec5
lemessurier <alemessurier@backflipstudios.com>
2014-05-16 16:57:11 -0600

    pvrtextool 4.6

--- pvrtextool-4.6-mac.rb
+++ pvrtextool-4.6-mac.rb
@@ -5,7 +5,7 @@ class Pvrtextool46Mac < Formula
   keg_only 'Anything that needs this will know where to look.'
 
   url 'http://brew.backflipstudios.com/brew/pvrtextool-4.6-mac.tar.gz'
-  sha1 '8ab1e7b12019012fe5780e290116c088ce6e4d5b'
+  sha1 '4099281307fd6a6f4842d416445ee42cd644b712'
   version '4.6'
 
   def install
