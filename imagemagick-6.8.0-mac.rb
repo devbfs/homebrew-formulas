b7341d48ffdddacb50c6841ac16e9a318c05c8b9
lemessurier <alemessurier@backflipstudios.com>
2014-05-29 16:27:55 -0600

    changed imagemagick keg to be keg_only, meaning it will no longer override the link to the default version of imagemagick

--- imagemagick-6.8.0-mac.rb
+++ imagemagick-6.8.0-mac.rb
@@ -2,6 +2,7 @@
 require 'formula'
 
 class Imagemagick680Mac < Formula
+  keg_only 'Anything that needs this will know where to find it.'
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/imagemagick-6.8.0-mac.tar.gz'
   sha1 'f57fe331563514906eaf7ce4f91dfd47f9dad24c'
