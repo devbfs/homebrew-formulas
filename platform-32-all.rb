b78f64d4d242a2b89d377367884630c67da58c32
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-05-30 17:14:23 -0600

    platform-32-all repackage (2)

--- platform-32-all.rb
+++ platform-32-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 
 class Platform32All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-32-all.tar.gz'
-  sha1 '1c9b7ee12e608484b273ff2175ce60b02f4a9a5d'
+  sha1 'fdf7e764d3c5975af7a8d7e01ae4d7f0ae7a54be'
   version '32'
 
   
