f748d438ce714524471ae874c9e50f35c8f57db9
lemessurier <alemessurier@backflipstudios.com>
2014-05-30 15:21:41 -0600

    removed static lib from libpng keg since the mac slice contained v1.4

--- libpng-1.5.10-all.rb
+++ libpng-1.5.10-all.rb
@@ -1,13 +1,14 @@
+
 require 'formula'
 
 class Libpng1510All < Formula
-  keg_only 'This is an in-house keg. Anything that needs it will know where to look.'
-
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/libpng-1.5.10-all.tar.gz'
-  sha1 'ac7217401412833f33c3092ce588fb909b2e241d'
+  sha1 '40237a667b3ea9484672a32cc05ab18fa45af489'
+  version '1.5.10'  
 
   def install
     prefix.install Dir['*']
   end
 end
+
