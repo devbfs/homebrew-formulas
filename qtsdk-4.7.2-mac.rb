d5ced1aec24e7682ecacf7cbf1c340fcc8ca5182
lemessurier <alemessurier@backflipstudios.com>
2014-06-02 11:35:21 -0600

    updated qtsdk keg so it contains exactly what the thirdparty dir does

--- qtsdk-4.7.2-mac.rb
+++ qtsdk-4.7.2-mac.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Qtsdk472Mac < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/qtsdk-4.7.2-mac.tar.gz'
-  sha1 'e5ca8d03702b486b86f7d38c172adcf86675f325'
+  sha1 '4cfeca69766cb08041c50e122994fda3c38b7174'
   version '4.7.2'
 
   def install
