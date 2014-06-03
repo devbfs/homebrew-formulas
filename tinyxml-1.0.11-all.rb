0d3f925efc26717d8988dae9ce26d9dbb82b098f
lemessurier <alemessurier@backflipstudios.com>
2014-06-03 14:08:40 -0600

    added android makefiles for zlib and tinyxml

--- tinyxml-1.0.11-all.rb
+++ tinyxml-1.0.11-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Tinyxml1011All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/tinyxml-1.0.11-all.tar.gz'
-  sha1 'b72fdc21ed861e7c8360853da9a7e5977266cfa6'
+  sha1 'f9f5e15051739d865b178e03f48cf3677439b50a'
 
   def install
     prefix.install Dir['*']
--- zlib-1.0-all.rb
+++ zlib-1.0-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Zlib10All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/zlib-1.0-all.tar.gz'
-  sha1 '74a3905f7828a4a6cb0dbe2c4d67fea025b56af3'
+  sha1 '0ec67d0cd6e6cefe0aa619c0af92618353d2b68c'
 
   def install
     prefix.install Dir['*']
