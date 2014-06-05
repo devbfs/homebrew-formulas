69760bddd79133624466ff20a0ebfd483146a2b2
lemessurier <alemessurier@backflipstudios.com>
2014-06-05 13:51:42 -0600

    bugfix in process artifacts script

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 'a4b9eee7871f949f9a5ee864c4f3bde69adc42e8'
-  version '17'
+  sha1 '0ffae5f365fe4348ac857cd876b6c233ae77e9ff'
+  version '18'
 
   def install
     prefix.install Dir['*']
