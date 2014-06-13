2b3e069832249ba026dc1c23206412af888ec252
lemessurier <alemessurier@backflipstudios.com>
2014-06-13 14:53:54 -0600

    corrected qtsdk 4.8.2 to be keg only

--- qtsdk-4.8.2-all.rb
+++ qtsdk-4.8.2-all.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Qtsdk482All < Formula
+  keg_only 'Anything that needs this will know where to look.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/qtsdk-4.8.2-all.tar.gz'
   sha1 '4ba365791109dc1ab4d3e0d578acafa25f75cbdc'
