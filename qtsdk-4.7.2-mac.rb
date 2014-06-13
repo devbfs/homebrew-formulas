3afab4eef9a3d4b288b472bb302c916d297ff1a3
lemessurier <alemessurier@backflipstudios.com>
2014-06-13 15:27:55 -0600

    corrected qtsdk 4.7.2 formula to be keg only

--- qtsdk-4.7.2-mac.rb
+++ qtsdk-4.7.2-mac.rb
@@ -2,6 +2,8 @@
 require 'formula'
 
 class Qtsdk472Mac < Formula
+  keg_only 'Anything that needs this will know where to find it.'
+
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/qtsdk-4.7.2-mac.tar.gz'
   sha1 '4cfeca69766cb08041c50e122994fda3c38b7174'
