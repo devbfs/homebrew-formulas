e0aace841bbdc0742267c4c1befb7c6ca79ce9a9
lemessurier <alemessurier@backflipstudios.com>
2014-06-24 10:41:58 -0600

    fixed formula name

--- libwebp.rb
+++ libwebp.rb
@@ -2,7 +2,7 @@
 require 'formula'
 
 class Libwebp < Formula
-  url 'http://brew.backflipstudios.com/brew/libwebp-1-all.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/libwebp.tar.gz'
   sha1 '5df74ea11ed480a695116d04cfe25f507fdaf6e2'
   version '2'
 
