c6db0c4ba612523381d8fbeab90ab3310713d267
lemessurier <alemessurier@backflipstudios.com>
2014-06-24 11:21:26 -0600

    re-deployed existing libwebp keg with new android makefile and same version number

--- libwebp-1-all.rb
+++ libwebp-1-all.rb
@@ -3,10 +3,8 @@ require 'formula'
 
 class Libwebp1All < Formula
   url 'http://brew.backflipstudios.com/brew/libwebp-1-all.tar.gz'
-  sha1 '2e1f207ad263643343654c4cb32ecfd606a8575d'
+  sha1 '577f8081999e370fa23d83ef84895e7d780ddbb2'
   version '1'
-
-  
 
   def install
     prefix.install Dir['*']
