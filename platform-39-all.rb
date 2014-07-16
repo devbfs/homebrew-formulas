a7127327060629bfd9a65e4b3328326183f07ecb
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-07-16 17:20:07 -0600

    platform-39-all update

--- platform-39-all.rb
+++ platform-39-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform39All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-39-all.tar.gz'
-  sha1 'eb363514930b37531f4d1a8040b1463e3d6a203b'
-  version '1'
+  sha1 'e78eb2f479c6bd9940d2ad6cad87112279ab8361'
+  version '2'
 
   
 
