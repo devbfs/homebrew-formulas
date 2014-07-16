32cf8efba162d6d8f2603629b37ad3aadec1ee5b
lemessurier <alemessurier@backflipstudios.com>
2014-07-15 18:12:33 -0600

    updated harfbuzz makefile to use an artifacts path rather than a cellar path, added dependency to freetype

--- harfbuzz-0.6.0-all.rb
+++ harfbuzz-0.6.0-all.rb
@@ -3,7 +3,9 @@ require 'formula'
 class Harfbuzz060All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/harfbuzz-0.6.0-all.tar.gz'
-  sha1 '23fd20c054d393358c229bed7d586d5998f9a7ed'
+  sha1 '7caa93ea28aaa694534bd65c42861c07afad78e0'
+
+  depends_on 'freetype-2.4.4-all'
 
   def install
     prefix.install Dir['*']
