b18ed7def0faa452cbe008494e125ac139ea9fc1
lemessurier <alemessurier@backflipstudios.com>
2014-07-17 17:53:32 -0600

    removed BF_ARTIFACTS from harfbuzz makefile and removed freetype keg dependency

--- harfbuzz-0.6.0-all.rb
+++ harfbuzz-0.6.0-all.rb
@@ -3,9 +3,7 @@ require 'formula'
 class Harfbuzz060All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/harfbuzz-0.6.0-all.tar.gz'
-  sha1 '7caa93ea28aaa694534bd65c42861c07afad78e0'
-
-  depends_on 'freetype-2.4.4-all'
+  sha1 'f0ec0bbd47259028609f79cd2660c5b317a9bd24'
 
   def install
     prefix.install Dir['*']
