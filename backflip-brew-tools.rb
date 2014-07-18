6a365609adac66df1d7ab102f32329c6d49f5fd0
lemessurier <alemessurier@backflipstudios.com>
2014-07-18 15:28:31 -0600

    deploying version 20 of brew tools

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,10 +3,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 '2bc7ca82a1123954c76c0520f86818ff0823529e'
-  version '19'
-
-  
+  sha1 'f2bc1433cc0356f6debdce34f113d17bb9519558'
+  version '20'
 
   def install
     prefix.install Dir['*']
