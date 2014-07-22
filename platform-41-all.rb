3b7fc30e70ed70224a189de20adc02af4baadc42
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-07-22 17:31:37 -0600

    platform-41-all

--- platform-41-all.rb
+++ platform-41-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform41All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-41-all.tar.gz'
-  sha1 'f380a21eb30ca33ef95851f88bac5f699c08a177'
-  version '1'
+  sha1 '68b3ca066544bcfadd740c28936eac13ad2e0fbd'
+  version '41'
 
   
 
