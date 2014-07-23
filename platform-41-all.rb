85d8c5b05e975d5292b40d841722e66cddf3a99e
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-07-23 15:44:25 -0600

    platform-41-all version 2

--- platform-41-all.rb
+++ platform-41-all.rb
@@ -3,10 +3,8 @@ require 'formula'
 
 class Platform41All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-41-all.tar.gz'
-  sha1 '6c3c835a8f7388ff70a41a6d882f55ac2618648a'
-  version '1'
-  keg_only 'Anything that needs this will know where to look'
-  homepage 'http://brew.backflipstudios.com'
+  sha1 'a427abf0e3e13bde3d0571620bf9402c62c36448'
+  version '2'
 
   
 
