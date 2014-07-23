2555ce67f6594168b9631444f88c082f3ea9ea09
lemessurier <alemessurier@backflipstudios.com>
2014-07-22 18:46:32 -0600

    deployed new version of backflip brew tools keg

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,9 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 'f2bc1433cc0356f6debdce34f113d17bb9519558'
-  version '20'
+  sha1 '2415adedce7d530e7a7e268b96b0d2e277046708'
+  
+  version '21'
 
   def install
     prefix.install Dir['*']
