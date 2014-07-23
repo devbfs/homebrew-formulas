96f87cc276c9db4f1b0875fc1347297392f61d76
lemessurier <alemessurier@backflipstudios.com>
2014-07-23 11:51:13 -0600

    minor bugfix in brew clean script

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,9 +3,9 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 '2415adedce7d530e7a7e268b96b0d2e277046708'
+  sha1 '33b1b305719701bfd7ca606079cc37f4cbe6ecb5'
   
-  version '21'
+  version '22'
 
   def install
     prefix.install Dir['*']
