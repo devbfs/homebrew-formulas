7403a3665af27c33c2bf1f3280377542f2dffbc2
lemessurier <alemessurier@backflipstudios.com>
2014-07-24 17:48:38 -0600

    path in protobuf script now points to opt

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Archon029All < Formula
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 'f26d9eca8d1bcfce1a00a25aff1601394f5d7262'
-  version '2'
+  sha1 'd4341af53e01d8c2c6666f996c2d6d76a52a7a8f'
+  version '3'
   keg_only 'Anything that needs this will know where to look'
   homepage 'http://brew.backflipstudios.com'
 
