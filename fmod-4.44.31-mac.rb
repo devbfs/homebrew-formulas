8f738d687b366eee58e30435ed5b966b285ef7f5
Shawn Harris <sharris@backflipstudios.com>
2014-07-24 13:37:33 -0600

    fmod-4.44.31-mac V2

--- fmod-4.44.31-mac.rb
+++ fmod-4.44.31-mac.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fmod44431Mac < Formula
   url 'http://brew.backflipstudios.com/brew/fmod-4.44.31-mac.tar.gz'
-  sha1 '7aeff99aa0b3adc0eae7c9665acdaf2e4dd0f71c'
-  version '1'
+  sha1 '2cd2426cf8f6d5c857cc4c8f2da187decb5b7af3'
+  version '2'
   keg_only 'Anything that needs this will know where to look'
   homepage 'http://brew.backflipstudios.com'
 
