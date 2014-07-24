443e900d509d9f59106eebce0540fb14f270ee44
lemessurier <alemessurier@backflipstudios.com>
2014-07-24 17:54:09 -0600

    corrected stupid path error in archon protobuf script

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Archon029All < Formula
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 'd4341af53e01d8c2c6666f996c2d6d76a52a7a8f'
-  version '3'
+  sha1 '200250bfc9a3d556dbf0897047359ef784ebab79'
+  version '4'
   keg_only 'Anything that needs this will know where to look'
   homepage 'http://brew.backflipstudios.com'
 
