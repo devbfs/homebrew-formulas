4fa7905bc1d3f5c62083e43fb48207fd44579c0c
lemessurier <alemessurier@backflipstudios.com>
2014-07-25 17:18:40 -0600

    new version of archon 0.2.9 with paths that no longer point to the cellar

--- archon-0.2.9-all.rb
+++ archon-0.2.9-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Archon029All < Formula
   url 'http://brew.backflipstudios.com/brew/archon-0.2.9-all.tar.gz'
-  sha1 '200250bfc9a3d556dbf0897047359ef784ebab79'
-  version '4'
+  sha1 '2fc2f5d99cead6ec17ed83326afa2556f667a110'
+  version '5'
   keg_only 'Anything that needs this will know where to look'
   homepage 'http://brew.backflipstudios.com'
 
