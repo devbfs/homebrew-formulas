17711a251e43f511f53031782ae828ed8ff6ff75
lemessurier <alemessurier@backflipstudios.com>
2014-07-25 16:44:13 -0600

    added keg-only marker to whefs keg

--- whefs-r338-all.rb
+++ whefs-r338-all.rb
@@ -1,7 +1,7 @@
-
 require 'formula'
 
 class WhefsR338All < Formula
+  keg_only 'Anything that needs this will know where to look'
   url 'http://brew.backflipstudios.com/brew/whefs-r338-all.tar.gz'
   sha1 'd81ea4f1f7e041e379e115e70a67c805abefe793'
   version '1'
