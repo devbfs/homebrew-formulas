d8644a876fe881ea93df66767af4d3533478fca1
lemessurier <alemessurier@backflipstudios.com>
2014-07-30 11:10:10 -0600

    fixed slight path bug in create script

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 '0795e6c0572eec84b4ca1a17fa202dff9b737e72'
-  version '27'
+  sha1 'e2d5ba9b34627e0ffe4566999b4e83f131244b49'
+  version '28'
 
   def install
     prefix.install Dir['*']
