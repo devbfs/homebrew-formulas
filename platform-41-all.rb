e856bd8a1bfbb4f15f650b1fe75b4bec555bc990
lemessurier <alemessurier@backflipstudios.com>
2014-07-30 11:43:49 -0600

    re-deployed platform 41 with the right hidden files included for android (.classpath)

--- platform-41-all.rb
+++ platform-41-all.rb
@@ -3,10 +3,8 @@ require 'formula'
 
 class Platform41All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-41-all.tar.gz'
-  sha1 'a427abf0e3e13bde3d0571620bf9402c62c36448'
-  version '2'
-
-  
+  sha1 '19f1ebad6ee4221a1105cbc3136f6e86a19bbb40'
+  version '3'
 
   def install
     prefix.install Dir['*']
