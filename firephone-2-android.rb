2a29d336f13e1cf15c242acfba90fd04da4f1573
lemessurier <alemessurier@backflipstudios.com>
2014-08-11 13:17:41 -0600

    added keg_only flag to firephone sdk r2 keg

--- firephone-2-android.rb
+++ firephone-2-android.rb
@@ -2,6 +2,7 @@
 require 'formula'
 
 class Firephone2Android < Formula
+  keg_only 'Anything that needs this will know where to look.'
   url 'http://brew.backflipstudios.com/brew/firephone-2-android.tar.gz'
   sha1 '86d35a8e2f53452588a5b98e764a8b21b026473f'
   version '1'  
