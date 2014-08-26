7f3083df7bb84f94eda00f9b28dc00830058d50a
Shawn Harris <sharris@backflipstudios.com>
2014-08-26 15:41:52 -0600

    Platform v45-2

--- platform-45-all.rb
+++ platform-45-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform45All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-45-all.tar.gz'
-  sha1 '9897e1492b84eb47d4d2baca50623b8097bbd2b1'
-  version '1'
+  sha1 '7f2cd14ab6fe0e21351de80b913bef0b336a3af7'
+  version '2'
 
   
 
