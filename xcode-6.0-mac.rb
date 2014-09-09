bdfe1fb97754266fe8c1d5bce9b519d705d9fb94
Jay Wagner <jwagner@backflipstudios.com>
2014-09-09 17:54:31 -0600

    Hand modified Xcode 6.0 to remove the docsets with really long file names.

--- xcode-6.0-mac.rb
+++ xcode-6.0-mac.rb
@@ -5,13 +5,13 @@ class Xcode60Mac < Formula
 
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/xcode-6.0-mac.tar.gz'
-  sha1 '39b590657b82da1ccf2551c118c4dd65e21ddbfa'
-  version '1'
+  sha1 'd9002f06deec379cf6a1322c3271ac93bfe8e153'
+  version '2'
 
   def install
     prefix.install Dir['*']
     puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
     puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
-    puts 'ln -s -f /usr/local/Cellar/xcode-6.0-mac/1/Xcode.app /Applications/Xcode.app'
+    puts 'ln -s -f /usr/local/Cellar/xcode-6.0-mac/2/Xcode.app /Applications/Xcode.app'
   end
 end
