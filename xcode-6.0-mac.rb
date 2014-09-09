459a4228defa46e597869fa1ca0464e449f7e4a0
Jay Wagner <jwagner@backflipstudios.com>
2014-09-09 15:27:58 -0600

    Adding Xcode 6.0 GM Seed formula.

---
+++ xcode-6.0-mac.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Xcode60Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/xcode-6.0-mac.tar.gz'
+  sha1 '39b590657b82da1ccf2551c118c4dd65e21ddbfa'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+    puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
+    puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
+    puts 'ln -s -f /usr/local/Cellar/xcode-6.0-mac/1/Xcode.app /Applications/Xcode.app'
+  end
+end
