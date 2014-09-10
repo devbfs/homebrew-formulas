17470271ad8ba53814202cefe6becd6e64ec8f50
Jay Wagner <jwagner@backflipstudios.com>
2014-09-09 20:55:48 -0600

    Trying a version of xcode with only the offending docset removed.

--- xcode-6.0-mac.rb
+++ xcode-6.0-mac.rb
@@ -5,13 +5,13 @@ class Xcode60Mac < Formula
 
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/xcode-6.0-mac.tar.gz'
-  sha1 'd9002f06deec379cf6a1322c3271ac93bfe8e153'
-  version '2'
+  sha1 '8be86238c9338441542fa607d32f0ad479e3b727'
+  version '3'
 
   def install
     prefix.install Dir['*']
     puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
     puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
-    puts 'ln -s -f /usr/local/Cellar/xcode-6.0-mac/2/Xcode.app /Applications/Xcode.app'
+    puts 'ln -s -f /usr/local/Cellar/xcode-6.0-mac/3/Xcode.app /Applications/Xcode.app'
   end
 end
