facb17dbe362feeb39f0e9a4135074772092292e
Jay Wagner <jwagner@backflipstudios.com>
2014-09-25 14:43:54 -0600

    Removing stupid extra character that was at the end of a line.

--- xcode-6.0.1-mac.rb
+++ xcode-6.0.1-mac.rb
@@ -13,5 +13,5 @@ class Xcode601Mac < Formula
     puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
     puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
     puts 'ln -s -f /usr/local/opt/xcode-6.0.1-mac/Xcode.app /Applications/Xcode.app'
-  end
+  end
 end
