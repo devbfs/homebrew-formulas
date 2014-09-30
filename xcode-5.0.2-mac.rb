6e2de93d9b1feee2ff15532969c80aa541e5bbdd
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-09-30 13:21:10 -0600

    Updated xcode formulas

--- xcode-5.0.2-mac.rb
+++ xcode-5.0.2-mac.rb
@@ -1,22 +1,22 @@
 require 'formula'
 
 class Xcode502Mac < Formula
-  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+  keg_only "No linking is necessary. Anything that needs to use this will know where to find it."
 
-  @xc_version = '5.0.2'
+  @xc_version = "5.0.2"
 
-  homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/xcode-#{xc_version}-mac.tar.gz'
-  sha1 '9300ab702e3916fef78209fbeab98457e19b281e'
-  version '1'
+  homepage "http://brew.backflipstudios.com"
+  url "http://brew.backflipstudios.com/brew/xcode-#{xc_version}-mac.tar.gz"
+  sha1 "9300ab702e3916fef78209fbeab98457e19b281e"
+  version "1"
 
   def install
     system "hdiutil", "attach", "-nobrowse", "xcode_#{xc_version}.dmg"
     system "cp", "-R", "/Volumes/Xcode/Xcode.app", "#{prefix}/Xcode.app"
     system "hdiutil", "detach", "-force", "/Volumes/Xcode"
 
-    puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
-    puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
-    puts 'ln -s -f /usr/local/opt/xcode-#{xc_version}-mac/Xcode.app /Applications/Xcode.app'
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Xcode, use this command:"
+    puts "ln -s -f /usr/local/opt/xcode-#{xc_version}-mac/Xcode.app /Applications/Xcode.app"
   end
 end
--- xcode-5.1-mac.rb
+++ xcode-5.1-mac.rb
@@ -1,21 +1,21 @@
 require 'formula'
 
 class Xcode51Mac < Formula
-  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+  keg_only "No linking is necessary. Anything that needs to use this will know where to find it."
 
-  @xc_version = '5.1'
+  @xc_version = "5.1"
 
-  homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/xcode-#{xc_version}-mac.tar.gz'
-  sha1 'bc589f315cf3ba6d1276ee38f9a38acd6ed65585'
-  version '1'
+  homepage "http://brew.backflipstudios.com"
+  url "http://brew.backflipstudios.com/brew/xcode-#{xc_version}-mac.tar.gz"
+  sha1 "bc589f315cf3ba6d1276ee38f9a38acd6ed65585"
+  version "1"
 
   def install
     system "hdiutil", "attach", "-nobrowse", "xcode_#{xc_version}.dmg"
     system "cp", "-R", "/Volumes/Xcode/Xcode.app", "#{prefix}/Xcode.app"
     system "hdiutil", "detach", "-force", "/Volumes/Xcode"
 
-    puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
-    puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
-    puts 'ln -s -f /usr/local/opt/xcode-#{xc_version}-mac/Xcode.app /Applications/Xcode.app'
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Xcode, use this command:"
+    puts "ln -s -f /usr/local/opt/xcode-#{xc_version}-mac/Xcode.app /Applications/Xcode.app"
 end
