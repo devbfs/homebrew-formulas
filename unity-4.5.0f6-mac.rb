58bca210829cd924a9a98b458612502a455668e3
Ambrose Krapacs <dev@backflipstudios.com>
2014-10-01 14:42:03 -0600

    Fixed unity formulas for new packages

--- unity-4.5.0f6-mac.rb
+++ unity-4.5.0f6-mac.rb
@@ -5,12 +5,22 @@ class Unity450f6Mac < Formula
 
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/unity-4.5.0f6-mac.tar.gz'
-  sha1 '022a9ce2949c3a454df684460a754e21f02d01c5'
+  sha1 '0cc418e301fbb4ccb0fbfb1277b1234b5ba2351d'
+  version '1'
 
   def install
     prefix.install Dir['*']
-    puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
-    puts 'Assuming there is no existing regular (non-brew) installation of Unity, use this command:'
-    puts 'ln -s -f /usr/local/opt/unity-4.5.0f6-mac /Applications/Unity'
+  end
+
+  def post_install
+    unity_version = "4.5.0f6"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
   end
 end
