aafc0690e5bd7d00fc9005283c9dfc7f1b18f4e7
Shawn Harris <sharris@backflipstudios.com>
2014-10-09 09:53:57 -0600

    Platform 49-2

--- platform-49-all.rb
+++ platform-49-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform49All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-49-all.tar.gz'
-  sha1 'c170a84f89fb04ed4956e3b66bee8900668d5dc2'
-  version '1'
+  sha1 '70ab936814f97776050e1f49448280cfb73b1d55'
+  version '2'
 
   
 
