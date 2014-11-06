675aaba0c5b3f76031a00d268631c68018be3ce8
Jay Wagner <jwagner@backflipstudios.com>
2014-11-05 17:32:49 -0700

    Adding Unity 4.6 beta 20 formula for D&D.

---
+++ unity-4.6.0b20-mac.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Unity460b20Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  url 'http://brew.backflipstudios.com/brew/unity-4.6.0b20-mac.tar.gz'
+  sha1 '369f1bbaeb00e75e71ddefe9202176df238e35c5'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def post_install
+    unity_version = "4.6.0b20"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
+  end
+end
