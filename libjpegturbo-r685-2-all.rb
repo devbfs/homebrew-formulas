93594c2635cc45c86f399af3af17c1137734279c
Shawn Harris <sharris@backflipstudios.com>
2014-11-26 16:07:33 -0700

    Fix up formula

--- libjpegturbo-r685-2-all.rb
+++ libjpegturbo-r685-2-all.rb
@@ -5,8 +5,9 @@ class LibjpegturboR6852All < Formula
   url 'http://brew.backflipstudios.com/brew/libjpegturbo-r685-2-all.tar.gz'
   sha1 'e4d2fd2a13bdfab0b94eb0b1958b152b4e5fe84b'
   version '1'
+  keg_only 'Anything that needs this will know where to look'
+  homepage 'http://brew.backflipstudios.com'
 
-  
 
   def install
     prefix.install Dir['*']
