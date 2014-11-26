829ed2b31b6a344a41ddb1a794a18845640d2df3
Shawn Harris <sharris@backflipstudios.com>
2014-11-26 16:09:22 -0700

    Fix up formula

--- bf_fontrendering-1-all.rb
+++ bf_fontrendering-1-all.rb
@@ -5,8 +5,9 @@ class Bf_fontrendering1All < Formula
   url 'http://brew.backflipstudios.com/brew/bf_fontrendering-1-all.tar.gz'
   sha1 'd7a454995f137738b839ec679ae41a54ae4f3eb4'
   version '1'
+  keg_only 'Anything that needs this will know where to look'
+  homepage 'http://brew.backflipstudios.com'
 
-  
 
   def install
     prefix.install Dir['*']
