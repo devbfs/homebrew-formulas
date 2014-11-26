df66c136e13b3b3f215c7da38ba0a75d83c7af81
Shawn Harris <sharris@backflipstudios.com>
2014-11-26 15:49:25 -0700

    Brew Tools V31

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 '5764d4070e554be61c1874567100130cd09f0a69'
-  version '30'
+  sha1 'aab063022b206561005a5cae81bbbb5b40fd14ed'
+  version '31'
 
   
 
