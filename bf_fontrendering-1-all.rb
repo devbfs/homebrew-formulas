0dcd220b56807fac109bb0b59d48f74d434cf35e
Shawn Harris <sharris@backflipstudios.com>
2014-11-26 16:11:59 -0700

    Remove underscore in class name

--- bf_fontrendering-1-all.rb
+++ bf_fontrendering-1-all.rb
@@ -1,7 +1,7 @@
 
 require 'formula'
 
-class Bf_fontrendering1All < Formula
+class BfFontrendering1All < Formula
   url 'http://brew.backflipstudios.com/brew/bf_fontrendering-1-all.tar.gz'
   sha1 'd7a454995f137738b839ec679ae41a54ae4f3eb4'
   version '1'
