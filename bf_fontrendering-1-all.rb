c59cda978efa3a54b62a19e8a3663faae94b4603
Shawn Harris <sharris@backflipstudios.com>
2014-12-02 17:48:15 -0700

    Bf_FontRendring-1-all V3

--- bf_fontrendering-1-all.rb
+++ bf_fontrendering-1-all.rb
@@ -3,10 +3,10 @@ require 'formula'
 
 class BfFontrendering1All < Formula
   url 'http://brew.backflipstudios.com/brew/bf_fontrendering-1-all.tar.gz'
-  sha1 'cac91b6e58d77f2d1855e6613c183df112002532'
-  version '2'
+  sha1 'e746b4e2d7ea40d05f00acbb2785b1910e63aaa9'
+  version '3'
 
-  
+
 
   def install
     prefix.install Dir['*']
