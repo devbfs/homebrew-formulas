0ac9abe1e2480d4516180a59d3599cae0221e89d
Shawn Harris <sharris@backflipstudios.com>
2014-12-11 12:22:02 -0700

    Platform V56-2

--- platform-56-all.rb
+++ platform-56-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform56All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-56-all.tar.gz'
-  sha1 '27b84307141385736f7469214897dff595efe1be'
-  version '1'
+  sha1 '9b37e899c489dd2a7e144b40dd6baa9b6e9c5973'
+  version '2'
 
   
 
