3fc7f22fba81c642c75dc4f1a53ef251180fa861
Jay Wagner <jwagner@backflipstudios.com>
2015-02-20 15:12:25 -0700

    Version 32 of backflip-brew-tools.  Adds keg-only option in bbt_create.py

--- backflip-brew-tools.rb
+++ backflip-brew-tools.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class BackflipBrewTools < Formula
   url 'http://brew.backflipstudios.com/brew/backflip-brew-tools.tar.gz'
-  sha1 'aab063022b206561005a5cae81bbbb5b40fd14ed'
-  version '31'
+  sha1 'b07795e32c2f1d1a45cbd7dced908c8278ac3c2a'
+  version '32'
 
   
 
