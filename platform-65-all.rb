19e48c3e332f15f484fb076f20a4dd5533481363
lemessurier <alemessurier@backflipstudios.com>
2015-03-20 13:51:15 -0600

    re-deploy of platform 65 with external url android fix

--- platform-65-all.rb
+++ platform-65-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform65All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-65-all.tar.gz'
-  sha1 '1621667f342d083cd20a4fba97513b91367f7efe'
-  version '1'
+  sha1 '50f86897947c5765610a7f5b38d4a83b5bb48d90'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
