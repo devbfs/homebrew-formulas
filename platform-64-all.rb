f71cb23a44a346c1f33851e8b4085d7b0ff41598
Kyle Roucis <kroucis@backflipstudios.com>
2015-03-25 11:58:31 -0600

    Platform V64-3

--- platform-64-all.rb
+++ platform-64-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform64All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-64-all.tar.gz'
-  sha1 'ac35156d8e89663e0dd254ef557cb1d77ce3141e'
-  version '2'
+  sha1 '9d25d0e4527ac05b4c233b1bc5488afe8b3f8b33'
+  version '3'
   keg_only 'Anything that needs this will know where to look.'
 
   
