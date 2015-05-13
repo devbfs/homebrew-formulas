d1f4fa750df32474e79441b3621200df416b7dbe
Kyle Roucis <kroucis@backflipstudios.com>
2015-05-13 15:34:57 -0600

    Platform V73-2

--- platform-73-all.rb
+++ platform-73-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform73All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-73-all.tar.gz'
-  sha1 'e418649d6cf72febf383c93c9e70c2440519965e'
-  version '1'
+  sha1 '43b164808aaf4d4579a36dfa197a61b07911fba0'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
