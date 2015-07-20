9727799d32fb7661ce36c6d81a6f534d1d92bad0
Shawn Harris <sharris@backflipstudios.com>
2015-07-20 15:43:44 -0600

    Platform V75-v3

--- platform-75-all.rb
+++ platform-75-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform75All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-75-all.tar.gz'
-  sha1 '5c88ac00e770e9393c3f738ed05c01cc51b6fa00'
-  version '2'
+  sha1 '8fa67d2a3d15d3577d19d2cb4e357c5b797a71da'
+  version '3'
   keg_only 'Anything that needs this will know where to look.'
 
   
