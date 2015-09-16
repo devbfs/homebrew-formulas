bba1181fa5eb81c63df68d8c4b4639e5f6bbd831
Jay Wagner <jwagner@backflipstudios.com>
2015-09-16 15:52:43 -0600

    Xcode 7.0

---
+++ xcode-7.0-mac.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Xcode70Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/xcode-7.0-mac.tar.gz'
+  sha1 '0b1b4dc84d649e003229c7ccf7b663c7c3298fcd'
+  version '1'
+
+  def install
+    xcode_version = "7.0"
+
+    system "hdiutil", "attach", "-nobrowse", "xcode_#{xcode_version}.dmg"
+    system "cp", "-R", "/Volumes/Xcode/Xcode.app", "#{prefix}/Xcode.app"
+    system "hdiutil", "detach", "-force", "/Volumes/Xcode"
+
+    puts 'You will need to manually create a symlink for this keg since modifying /Applications requires root permissions.'
+    puts 'Assuming you do not have a regular (non-brew) installation of Xcode, use this command:'
+    puts 'ln -s -f /usr/local/opt/xcode-#{xcode_version}-mac/Xcode.app /Applications/Xcode.app'
+  end
+end
