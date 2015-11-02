18dd34dddf5edab6ada2f7597f2b1d751fde98c3
Shawn Harris <sharris@backflipstudios.com>
2015-11-02 13:46:39 -0700

    Platform V81 9

--- platform-81-all.rb
+++ platform-81-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform81All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-81-all.tar.gz'
-  sha1 'db0def78773f98b22d70b63713f5ec805f7f1fac'
-  version '8'
+  sha1 '2d15116103bbc88587b99947db37e9caad926cba'
+  version '9'
   keg_only 'Anything that needs this will know where to look.'
 
   
