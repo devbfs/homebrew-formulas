7751b9b57f89bfa23ab92c1d8cc2a22d9009570f
Jay Wagner <jwagner@backflipstudios.com>
2015-11-17 15:28:36 -0700

    Unity 5.2.2f1 Backflip v1

---
+++ unity-5.2.2f1-backflip-v1-mac.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Unity522f1-backflip-v1Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  url 'http://brew.backflipstudios.com/brew/unity-5.2.2f1-backflip-v1-mac.tar.gz'
+  sha1 '3bb9f6002e4bb72b763d1ebd1b8ff849e7b0f91b'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def post_install
+    unity_version = "5.2.2f1-backflip-v1"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
+  end
+end
