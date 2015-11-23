f0fe311ddd3a50ae75d7f4bd2ef569d74579532c
Shawn Harris <sharris@backflipstudios.com>
2015-11-23 16:12:01 -0700

    Platform V82 patch 2

--- platform-82-all.rb
+++ platform-82-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform82All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-82-all.tar.gz'
-  sha1 '1e23d718d394863cb51ea5cf9f377037aac63474'
-  version '1'
+  sha1 'e912cd57b5cd037d183e592b64ba5bb4cc9a4321'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
