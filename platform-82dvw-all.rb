c7d61a9871975ed778fb4d3c03afe5f1e7c8c15d
Shawn Harris <sharris@backflipstudios.com>
2015-12-08 13:52:45 -0700

    Platform V82dvw brew version 2

--- platform-82dvw-all.rb
+++ platform-82dvw-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform82dvwAll < Formula
   url 'http://brew.backflipstudios.com/brew/platform-82dvw-all.tar.gz'
-  sha1 '4108a0c83d2f5d478417ccf21b41dc0ca73b7495'
-  version '1'
+  sha1 'c1dd690596de5c0457475c02988eb38903e7adc2'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
