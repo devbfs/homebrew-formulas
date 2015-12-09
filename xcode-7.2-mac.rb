b7b42045b3545a9489d1cee1b4a117387907f41a
Ryan Harper <rharper@backflipstudios.com>
2015-12-08 18:03:39 -0700

    added formula for Xcode 7.2 keg

---
+++ xcode-7.2-mac.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Xcode7Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  homepage 'http://brew.backflipstudios.com'
+  url 'http://brew.backflipstudios.com/brew/xcode-7.2-mac.tar.gz'
+  sha1 '260218d62b0db6b5c2ebaa63ae1f2b4b8d86dfa3'
+  version '1'
+
+  def install
+    xcode_version = "7.2"
+
+    system "hdiutil", "attach", "-nobrowse", "xcode_#{xcode_version}.dmg"
+    system "rsync", "-a", "--exclude=Contents/Developer/Applications/Simulator*", "/Volumes/Xcode/Xcode.app/", "#{prefix}/Xcode.app"
+    system "hdiutil", "detach", "-force", "/Volumes/Xcode"
+
+    puts '⚠️DO NOT USE TO INSTALL XCODE ON ANYTHING BUT A BUILD AGENT⚠️'
+  end
+end
