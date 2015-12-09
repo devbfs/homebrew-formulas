d5286fe6fc29c6e4ce6765678958f750d2c80677
Ryan Harper <rharper@backflipstudios.com>
2015-12-08 18:11:18 -0700

    fixed class name for Xcode 7.2 formula

--- xcode-7.2-mac.rb
+++ xcode-7.2-mac.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Xcode7Mac < Formula
+class Xcode72Mac < Formula
   keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
 
   homepage 'http://brew.backflipstudios.com'
