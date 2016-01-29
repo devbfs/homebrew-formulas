4fd6d8dd3e56ea3d3428248a8cb546ebee749159
Jay Wagner <jwagner@backflipstudios.com>
2016-01-28 18:14:50 -0700

    Unity 5.3.2p1

---
+++ unity-5.3.2p1-mac.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Unity532p1Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  url 'http://brew.backflipstudios.com/brew/unity-5.3.2p1-mac.tar.gz'
+  sha1 'b840d65e0871846c7e3169d34ea31077f1c51238'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def post_install
+    unity_version = "5.3.2p1"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
+  end
+end
