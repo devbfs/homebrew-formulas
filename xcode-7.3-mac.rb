d626ae70a746d6d1c56ff5424e75341a46a7416b
Ryan Harper <rharper@backflipstudios.com>
2016-03-22 16:05:16 -0600

    added additional exclude

--- xcode-7.3-mac.rb
+++ xcode-7.3-mac.rb
@@ -12,7 +12,13 @@ class Xcode73Mac < Formula
     xcode_version = "7.3"
 
     system "hdiutil", "attach", "-nobrowse", "xcode_#{xcode_version}.dmg"
-    system "rsync", "-a", "--exclude=Contents/Developer/Applications/Simulator*", "--exclude=Contents/Developer/Platforms/AppleTVSimulator.platform", "--exclude=Contents/Applications/Application*", "--exclude=Contents/Developer/Library/PrivateFrameworks/CoreSimulator.framework/Versions/A/CoreSimulator", "/Volumes/Xcode/Xcode.app/", "#{prefix}/Xcode.app"
+    system "rsync", "-a",
+      "--exclude=Contents/Developer/Applications/Simulator*",
+      "--exclude=Contents/Developer/Platforms/AppleTVSimulator.platform",
+      "--exclude=Contents/Applications/Application*",
+      "--exclude=Contents/Developer/Library/PrivateFrameworks/CoreSimulator.framework/Versions/A/CoreSimulator",
+      "--exclude=Contents/Developer/Platforms/AppleTVOS.platform/Developer/Library/PrivateFrameworks/GPUToolsMobileKit.framework/Versions/A/GPUToolsMobileKit",
+      "/Volumes/Xcode/Xcode.app/", "#{prefix}/Xcode.app"
     system "hdiutil", "detach", "-force", "/Volumes/Xcode"
 
     puts '⚠️DO NOT USE TO INSTALL XCODE ON ANYTHING BUT A BUILD AGENT⚠️'
