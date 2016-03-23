8182d1bc979b12ecec3dbcca6c792186bb318bc7
Shawn Harris <sharris@backflipstudios.com>
2016-03-23 13:44:04 -0600

    Platform v86 p2 Brew version 2

--- platform-86p2-all.rb
+++ platform-86p2-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform86p2All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-86p2-all.tar.gz'
-  sha1 '1322a850c938312e926325dce9a060b2b1cb04c2'
-  version '1'
+  sha1 'cf628e95fba1bea713683934143c7a357a60a666'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
