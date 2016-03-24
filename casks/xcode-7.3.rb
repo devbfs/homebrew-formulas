b41a8fd1dc632a91c91eb3e2dbcab1fb65573d0c
Ryan Harper <rharper@backflipstudios.com>
2016-03-24 15:33:54 -0600

    removed formula for Xcode 7.3 and created first attempt at cask formula for Xcode 7.3

---
+++ casks/xcode-7.3.rb
@@ -0,0 +1,11 @@
+cask 'xcode73' do
+  version '1'
+  sha256 '13706d5a9cf74c798d3ca794f0ebae3e344f913422d2bb7d1ddd3bca331e49b5'
+
+  url "http://brew.backflipstudios.com/brew/Xcode_7.3.dmg"
+  name 'Xcode 7.3'
+  homepage 'http://brew.backflipstudios.com/'
+  license :closed
+
+  app 'Xcode.app'
+end
