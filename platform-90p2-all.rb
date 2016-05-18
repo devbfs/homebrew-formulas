dac3b2b5e9147ade8117bcdb7453d5a8958d83fd
Jeremy Anderson <janderson@backflipstudios.com>
2016-05-18 11:51:04 -0600

    Platform V90 p2 again and again

--- platform-90p2-all.rb
+++ platform-90p2-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform90p2All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-90p2-all.tar.gz'
-  sha1 '6b32d4508e7284a4293b2b95bdf78c0d76a0f42b'
-  version '2'
+  sha256 '26390e881bcd6a623f8d9754cd8ef2d02eba67fefa37e04ecdb463244430e911'
+  version '3'
   keg_only 'Anything that needs this will know where to look.'
 
   
