3d6f634b16f795f54d3a8328d5e74cfaa0eada08
Shawn Harris <sharris@backflipstudios.com>
2016-06-23 16:17:02 -0600

    Platform v92 p2 b2

--- platform-92p2-all.rb
+++ platform-92p2-all.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Platform92p2All < Formula
   url 'http://brew.backflipstudios.com/brew/platform-92p2-all.tar.gz'
-  sha256 'b3777737facab5aa55795e0ec59288db8e0052886a731d8ca93a697dc8096dfe'
-  version '1'
+  sha256 '3e496b7dd292d1560281dc01f10985c3f5391f7a710b9964223e1c55392ba72f'
+  version '2'
   keg_only 'Anything that needs this will know where to look.'
 
   
