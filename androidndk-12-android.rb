aebada548412dd2e7a6a019be468c5cfdf191573
Jay Wagner <jwagner@backflipstudios.com>
2016-06-23 14:34:32 -0600

    Correct URL to NDK 12

--- androidndk-12-android.rb
+++ androidndk-12-android.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Androidndk12Android < Formula
   homepage 'http://brew.backflipstudios.com'
-  url 'http://brew.backflipstudios.com/brew/androidndk-10e-android.tar.gz'
+  url 'http://brew.backflipstudios.com/brew/androidndk-12-android.tar.gz'
   sha256 '5c48a74b3a46ddaee2535847176772a114fb7241c19c3768495733607a53a11a'
 
   def install
