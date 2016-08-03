454526926daba9a246d5bcb4f0fbb74c3b256642
Jay Wagner <jwagner@backflipstudios.com>
2016-08-03 15:01:58 -0600

    Add Unity 5.3.6f1 formula

---
+++ unity-5.3.6f1-mac.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Unity536f1Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  url 'http://brew.backflipstudios.com/brew/unity-5.3.6f1-mac.tar.gz'
+  sha256 'a04947a91bc28f85982e2c28b341076b0c35c0bef2e0ed9d19a5fbccb5bc2fb3'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def post_install
+    unity_version = "5.3.6f1"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
+  end
+end
