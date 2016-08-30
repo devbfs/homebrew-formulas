05faed27032349376fa7350a6030c3bd66688955
Jay Wagner <jwagner@backflipstudios.com>
2016-08-30 16:00:07 -0600

    Add Xcode 8 beta 6 cask

---
+++ Casks/xcode-8.b6.rb
@@ -0,0 +1,11 @@
+cask 'xcode-8.b6' do
+  version '1'
+  sha256 '43cd4e7fb5a6e669f30f715f1af9dec672828a864e86f229a074b4220a30d63d'
+
+  url "http://brew.backflipstudios.com/brew/Xcode_8.b6.dmg"
+  name 'Xcode 8.b6'
+  homepage 'http://brew.backflipstudios.com/'
+  license :closed
+
+  app 'Xcode_8.b6.app'
+end
