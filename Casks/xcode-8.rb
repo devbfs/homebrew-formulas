730d7991d1bbc9d2b34e680e46635f1275d29431
Jay Wagner <jwagner@backflipstudios.com>
2016-09-13 14:11:26 -0600

    Add Xcode 8 GM.

---
+++ Casks/xcode-8.rb
@@ -0,0 +1,11 @@
+cask 'xcode-8' do
+  version '1'
+  sha256 'ffef6c1831d02df4684f6e0ec310392c6566a2ae8972ade57372c10bf0bb7222'
+
+  url "http://brew.backflipstudios.com/brew/Xcode_8.dmg"
+  name 'Xcode 8'
+  homepage 'http://brew.backflipstudios.com/'
+  license :closed
+
+  app 'Xcode_8.app'
+end
