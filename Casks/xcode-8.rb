366e83f391bd647f7bbcecd50d9c73a2f7bc9078
Jay Wagner <jwagner@backflipstudios.com>
2016-09-13 14:52:24 -0600

    Update SHA256 for Xcode 8.

--- Casks/xcode-8.rb
+++ Casks/xcode-8.rb
@@ -1,6 +1,6 @@
 cask 'xcode-8' do
   version '1'
-  sha256 'ffef6c1831d02df4684f6e0ec310392c6566a2ae8972ade57372c10bf0bb7222'
+  sha256 '7a0bcd84cd612805f94dc65fd801d82ead900609dabaec220f576aee6ed32322'
 
   url "http://brew.backflipstudios.com/brew/Xcode_8.dmg"
   name 'Xcode 8'
