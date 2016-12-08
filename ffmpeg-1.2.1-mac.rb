27b1daabfc903a07d55b0595ec1effe73aa2b639
Jay Wagner <jwagner@backflipstudios.com>
2016-12-08 11:13:24 -0700

    Update SHA to use 256-bit algorithm

--- ffmpeg-1.2.1-mac.rb
+++ ffmpeg-1.2.1-mac.rb
@@ -1,13 +1,11 @@
-
 require 'formula'
 
 class Ffmpeg121Mac < Formula
-
+  homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/ffmpeg-1.2.1-mac.tar.gz'
-  sha1 'c1322b87ce4f4d179054cfafe4e9de83dc2bafe1'
+  sha256 'c6b8f34bc6d0f99368ebc29c6ebb3b8c12b9f09790e8695276417a23a9b204e5'
   version '1'
   keg_only 'Anything that needs this will know where to look'
-  homepage 'http://brew.backflipstudios.com'
 
   def install
     prefix.install Dir['*']
