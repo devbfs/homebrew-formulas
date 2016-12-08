0cecf3bb4d74f965ddfd57c72d2fdd31e00d74c1
Jay Wagner <jwagner@backflipstudios.com>
2016-12-08 12:00:02 -0700

    Fix SHA256

--- fmod-4.36.04-mac.rb
+++ fmod-4.36.04-mac.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fmod43604Mac < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/fmod-4.36.04-mac.tar.gz'
-  sha256 '5080529a5187b4c93804e4664590842d3155448a'
+  sha256 '738446aa2a41735cb53a43e221952625dd562fe0911037d2f9721ae0bb49f0da'
   version '1'
   keg_only 'Anything that needs this will know where to look'
 
