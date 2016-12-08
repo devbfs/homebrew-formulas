5e638f05bb281421039fe6a0e4c2184bccf7c977
Jay Wagner <jwagner@backflipstudios.com>
2016-12-08 11:56:20 -0700

    Fix SHA256

--- protobuf-2.4.1-all.rb
+++ protobuf-2.4.1-all.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Protobuf241All < Formula
   homepage 'http://brew.backflipstudios.com'
   url 'http://brew.backflipstudios.com/brew/protobuf-2.4.1-all.tar.gz'
-  sha256 '28f8c2029d385e34d015b2fac19ffeb15e5e5063ec8ef1047e7557c577d214cb'
+  sha256 '1d693f7e29e4be713f453b82cad50fb9d0ded78d5ed9e5947c75e7cd6ab1f8c7'
   version '1'
   keg_only 'Anything that needs this will know where to look'
 
