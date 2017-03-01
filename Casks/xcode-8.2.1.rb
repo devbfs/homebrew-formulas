04fc87f2afd09404b49b9a7c04649daa39bfc208
Ryan Harper <rharper@backflipstudios.com>
2017-02-28 18:07:58 -0700

    Xcode 8.2.1

---
+++ Casks/xcode-8.2.1.rb
@@ -0,0 +1,11 @@
+cask 'xcode-821' do
+  version '1'
+  sha256 'bdaa76ec732291f9d49b274325bc179188f47fd2824841b8e6d35fee651836ac'
+
+  url "http://brew.backflipstudios.com/brew/Xcode_8.2.1.dmg"
+  name 'Xcode 8.2.1'
+  homepage 'http://brew.backflipstudios.com/'
+  license :closed
+
+  app 'Xcode.app'
+end
