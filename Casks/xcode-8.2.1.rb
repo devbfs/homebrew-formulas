cdc8d25fb53a170c52cc1066db6e77ccd9dd1caa
Jay Wagner <jwagner@backflipstudios.com>
2017-04-12 15:35:56 -0600

    Update SHA sum for Xcode 8.2.1

--- Casks/xcode-8.2.1.rb
+++ Casks/xcode-8.2.1.rb
@@ -1,6 +1,6 @@
 cask 'xcode-8.2.1' do
-  version '1'
-  sha256 'bdaa76ec732291f9d49b274325bc179188f47fd2824841b8e6d35fee651836ac'
+  version '2'
+  sha256 '3ad976e3ed68c1a7d350c44b65efb7688d9ceae79efa4fa18b7b70e19dcf4f8f'
 
   url "http://brew.backflipstudios.com/brew/Xcode_8.2.1.dmg"
   name 'Xcode 8.2.1'
