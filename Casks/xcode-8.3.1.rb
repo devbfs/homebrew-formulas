6383e1422212deedbba41b970205767b508e296b
Jay Wagner <jwagner@backflipstudios.com>
2017-04-18 21:44:10 -0600

    Add checksum for Xcode 8.3.1

--- Casks/xcode-8.3.1.rb
+++ Casks/xcode-8.3.1.rb
@@ -1,7 +1,7 @@
 cask 'xcode-8.3.1' do
     xcode_version = '8.3.1'
     version '1'
-    sha256 ''
+    sha256 'd370e2ec0ef1d36e7d3d2bf92743f072edb29740d1c194a29b1c3a1a849d40fc'
 
     url "http://brew.backflipstudios.com/brew/Xcode_#{xcode_version}.dmg"
     name "Xcode #{xcode_version}"
