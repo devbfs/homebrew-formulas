091e2a5d8a6edcf28a5af4c1e75e483489bc14b2
Jay Wagner <jwagner@backflipstudios.com>
2017-07-27 16:34:29 -0600

    Android NDK 15b cask

---
+++ Casks/androidndk-15b-android.rb
@@ -0,0 +1,43 @@
+cask 'androidndk-15b-android' do
+  version '15b'
+  sha256 'c05012c5562013a5773ecfab442e41e9413a01819ae210a84e60bc98e5587dac'
+
+  # dl.google.com/android/repository/android-ndk was verified as official when first introduced to the cask
+  url "https://dl.google.com/android/repository/android-ndk-r#{version}-darwin-x86_64.zip"
+  name 'Android NDK'
+  homepage 'https://developer.android.com/ndk/index.html'
+
+  conflicts_with cask: 'android-ndk'
+  conflicts_with cask: 'crystax-ndk'
+
+  linked_path = "#{HOMEBREW_PREFIX}/share/androidndk-#{version}-android"
+
+  # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/ndk_exec.sh"
+  preflight do
+    FileUtils.ln_sf("#{staged_path}/android-ndk-r#{version}", linked_path)
+
+    IO.write shimscript, <<-EOS.undent
+      #!/bin/bash
+      readonly executable="#{staged_path}/android-ndk-r#{version}/$(basename ${0})"
+      test -f "${executable}" && exec "${executable}" "${@}"
+    EOS
+  end
+
+  [
+    'ndk-build',
+    'ndk-depends',
+    'ndk-gdb',
+    'ndk-stack',
+    'ndk-which',
+  ].each { |link_name| binary shimscript, target: link_name }
+
+  uninstall_postflight do
+    FileUtils.rm(linked_path)
+  end
+
+  caveats <<-EOS.undent
+   You may want to add to your profile:
+      'export ANDROID_NDK_HOME="#{linked_path}"'
+  EOS
+end
