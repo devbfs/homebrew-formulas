7f75f500c17419cd6ac12e89493d5eb0843a82ac
Jay Wagner <jwagner@backflipstudios.com>
2017-08-03 18:44:36 -0600

    Xcode 9 beta 4

---
+++ Casks/xcode-9b4.rb
@@ -0,0 +1,12 @@
+cask 'xcode-9b4' do
+    xcode_version = '9.b4'
+    version '1'
+    sha256 'ab6848b51885017547221193fa57c44d5b8c636f12608389dd98c276fd38c894'
+
+    url "http://brew.backflipstudios.com/brew/Xcode_#{xcode_version}.dmg"
+    name "Xcode #{xcode_version}"
+    homepage 'http://brew.backflipstudios.com/'
+    license :closed
+
+    app "Xcode_#{xcode_version}.app"
+end
