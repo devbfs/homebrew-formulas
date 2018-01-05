1c4440a3672ba2bafbb454591757dfe1a5fb92ac
Jay Wagner <jwagner@backflipstudios.com>
2018-01-05 15:10:58 -0700

    Unity 2017.2.1f1

---
+++ unity-2017.2.1f1-mac.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Unity201721f1Mac < Formula
+  keg_only 'No linking is necessary. Anything that needs to use this will know where to find it.'
+
+  url 'http://brew.backflipstudios.com/brew/unity-2017.2.1f1-mac.tar.gz'
+  sha256 '5be311e337f563ed45ec3690ad669a3fcb4c14f8481191cc426e1648d227a91a'
+  version '1'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def post_install
+    unity_version = "2017.2.1f1"
+
+    system "hdiutil", "attach", "-nobrowse", "/usr/local/opt/unity-#{unity_version}-mac/unity-#{unity_version}.dmg"
+    system "cp", "-R", "/Volumes/unity-#{unity_version}/Unity", "/usr/local/opt/unity-#{unity_version}-mac/Unity"
+    system "hdiutil", "detach", "-force", "/Volumes/unity-#{unity_version}"
+
+    puts "You will need to manually create a symlink for this keg since modifying /Applications requires root permissions."
+    puts "Assuming you do not have a regular (non-brew) installation of Unity, use this command:"
+    puts "ln -s -f /usr/local/opt/unity-#{unity_version}-mac/Unity /Applications/Unity"
+  end
+end
