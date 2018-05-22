de89064ca1a4890d5c177f1c240d13741a202348
Jay Wagner <jwagner@backflipstudios.com>
2018-05-22 16:08:21 -0600

    Update to Xcode 9.3 cask

--- Casks/xcode-9.3.rb
+++ Casks/xcode-9.3.rb
@@ -1,9 +1,9 @@
 cask 'xcode-9.3' do
     xcode_version = '9.3'
-    version '1'
-    sha256 '28ef8bacdef59262155b682c7357e6d70fdbb41d9eeb433103057544994113f7'
+    version :latest
+    sha256 :no_check
 
-    url "http://brew.backflipstudios.com/brew/Xcode_#{xcode_version}.dmg"
+    url "http://brew.backflipstudios.com/brew/Xcode_#{xcode_version}.zip"
     name "Xcode #{xcode_version}"
     homepage 'http://brew.backflipstudios.com/'
 
