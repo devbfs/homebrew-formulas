522ed0a28aaefde86e0fb7b22d593dba47deb50c
Jay Wagner <jwagner@backflipstudios.com>
2019-03-04 17:05:07 -0700

    Update Xcode 10.1 sha

--- Casks/xcode-10.1.rb
+++ Casks/xcode-10.1.rb
@@ -1,7 +1,7 @@
 cask 'xcode-10.1' do
     xcode_version = '10.1'
     version :latest
-    sha256 ''
+    sha256 'b8fc4b4409bdf481ad509af95c4ebcc3ad90c63105f52a4a9906791c56304466'
 
     url "http://brew.backflipstudios.com/brew/Xcode_#{xcode_version}.zip"
     name "Xcode #{xcode_version}"
